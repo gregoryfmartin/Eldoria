f13be4591d08339a4c78615fd8ecb211896f02fa
Gregory F Martin <greg@gregfmartin.org>
2023-04-17 22:45:31 -0400

    Typo

--- ClassApproach.ps1
+++ ClassApproach.ps1
@@ -8754,7 +8754,7 @@ Class MTOTree : MapTileObject {
         Note the pattern here for the params. In order for state changes to work, the ScriptBlock will need to have two arguments: 
         A reference to the object itself, and the source. AFAIK, this is because of how the ScriptBlock gets invoked. The $this reference
         doesn't work as it references the CommandWindow instance rather than the owning object (in this case, MTOTree). Because of this
-        somewhate counterintuitive nature, the caller (in this case, the 'use' command) will invoke the ScriptBlock with two arguments that
+        somewhat counterintuitive nature, the caller (in this case, the 'use' command) will invoke the ScriptBlock with two arguments that
         match the signature here. State changes can be inflicted upon Self (passed as a reference), and Source gets removed from the Player's
         Inventory.
         #>
