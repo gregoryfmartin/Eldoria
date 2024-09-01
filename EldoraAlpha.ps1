4009bbfa30a9465ec38d212ac2610043fd27c4e5
Gregory F Martin <greg@gregfmartin.org>
2024-09-01 10:53:02 -0400

    Finished Status Hud Window

--- EldoraAlpha.ps1
+++ EldoraAlpha.ps1
@@ -19172,6 +19172,237 @@ Class BattleEnemyImageWindow : WindowBase {
 
 ###############################################################################
 #
+# STATUS HUD WINDOW
+#
+###############################################################################
+Class StatusHudWindow : WindowBase {
+    Static [Int]$WindowLTRow    = 1
+    Static [Int]$WindowLTColumn = 1
+    Static [Int]$WindowRBRow    = 3
+    Static [Int]$WindowRBColumn = 80
+
+    Static [String]$WindowBorderHorizontal = '*-------------------------------------------------------------------------------*'
+    Static [String]$WindowBorderLeft       = '|'
+    Static [String]$WindowBorderRight      = '|'
+    Static [String]$LineBlankStr           = '                                                                              '
+
+    [Boolean]$StatLineDrawDirty
+    [ATString]$LineBlank
+    [ATCoordinates]$StatLineDrawCoords
+    [ATCoordinates]$StatSeparatorDrawCoords
+    [ATStringComposite]$StatLineActual
+
+    StatusHudWindow() {
+        $this.LeftTop = [ATCoordinates]@{
+            Row    = [StatusHudWindow]::WindowLTRow
+            Column = [StatusHudWindow]::WindowLTColumn
+        }
+        $this.RightBottom = [ATCoordinates]@{
+            Row    = [StatusHudWindow]::WindowRBRow
+            Column = [StatusHudWindow]::WindowRBColumn
+        }
+        $this.BorderDrawColors = [ConsoleColor24[]](
+            [CCWhite24]::new(),
+            [CCWhite24]::new(),
+            [CCWhite24]::new(),
+            [CCWhite24]::new()
+        )
+        $this.BorderStrings = [String[]](
+            [StatusHudWindow]::WindowBorderHorizontal,
+            [StatusHudWindow]::WindowBorderHorizontal,
+            [StatusHudWindow]::WindowBorderLeft,
+            [StatusHudWindow]::WindowBorderRight
+        )
+        $this.UpdateDimensions()
+
+        $this.StatLineDrawDirty = $true
+        $this.LineBlank = [ATString]@{
+            UserData   = [StatusHudWindow]::LineBlankStr
+            UseATReset = $true
+        }
+        $this.StatLineDrawCoords = [ATCoordinates]@{
+            Row    = 2
+            Column = 3
+        }
+        $this.StatSeparatorDrawCoords = [ATCoordinates]@{
+            Row    = 2
+            Column = [StatusHudWindow]::WindowRBColumn - 48
+        }
+    }
+
+    [Void]Draw() {
+        ([WindowBase]$this).Draw()
+
+        If($this.StatLineDrawDirty -EQ $true) {
+            $this.ComposeStatLineString()
+            $this.LineBlank.Prefix.Coordinates = $this.StatLineDrawCoords
+            Write-Host "$($this.LineBlank.ToAnsiControlSequenceString())$($this.StatLineActual.ToAnsiControlSequenceString())"
+            $this.StatLineDrawDirty = $false
+        }
+    }
+
+    [Void]ComposeStatLineString() {
+        [String]$AtkStatFmtStr = ''
+        [String]$DefStatFmtStr = ''
+        [String]$MatStatFmtStr = ''
+        [String]$MdfStatFmtStr = ''
+        [String]$SpdStatFmtStr = ''
+        [String]$AccStatFmtStr = ''
+        [String]$LckStatFmtStr = ''
+        [String]$AtkDispStr    = 'ATK:'
+        [String]$DefDispStr    = 'DEF:'
+        [String]$MatDispStr    = 'MAT:'
+        [String]$MdfDispStr    = 'MDF:'
+        [String]$SpdDispStr    = 'SPD:'
+        [String]$AccDispStr    = 'ACC:'
+        [String]$LckDispStr    = 'LCK:'
+
+        If($Script:ThePlayer.Stats[[StatId]::Attack].Base -LT 10) {
+            $AtkStatFmtStr = "{0:d2}" -F $Script:ThePlayer.Stats[[StatId]::Attack].Base
+        } Elseif($Script:ThePlayer.Stats[[StatId]::Attack].Base -GE 10) {
+            $AtkStatFmtStr = "$($Script:ThePlayer.Stats[[StatId]::Attack].Base)"
+        }
+        If($Script:ThePlayer.Stats[[StatId]::Defense].Base -LT 10) {
+            $DefStatFmtStr = "{0:d2}" -F $Script:ThePlayer.Stats[[StatId]::Defense].Base
+        } Elseif($Script:ThePlayer.Stats[[StatId]::Defense].Base -GE 10) {
+            $DefStatFmtStr = "$($Script:ThePlayer.Stats[[StatId]::Defense].Base)"
+        }
+        If($Script:ThePlayer.Stats[[StatId]::MagicAttack].Base -LT 10) {
+            $MatStatFmtStr = "{0:d2}" -F $Script:ThePlayer.Stats[[StatId]::MagicAttack].Base
+        } Elseif($Script:ThePlayer.Stats[[StatId]::MagicAttack].Base -GE 10) {
+            $MatStatFmtStr = "$($Script:ThePlayer.Stats[[StatId]::MagicAttack].Base)"
+        }
+        If($Script:ThePlayer.Stats[[StatId]::MagicDefense].Base -LT 10) {
+            $MdfStatFmtStr = "{0:d2}" -F $Script:ThePlayer.Stats[[StatId]::MagicDefense].Base
+        } Elseif($Script:ThePlayer.Stats[[StatId]::MagicDefense].Base -GE 10) {
+            $MdfStatFmtStr = "$($Script:ThePlayer.Stats[[StatId]::MagicDefense].Base)"
+        }
+        If($Script:ThePlayer.Stats[[StatId]::Speed].Base -LT 10) {
+            $SpdStatFmtStr = "{0:d2}" -F $Script:ThePlayer.Stats[[StatId]::Speed].Base
+        } Elseif($Script:ThePlayer.Stats[[StatId]::Speed].Base -GE 10) {
+            $SpdStatFmtStr = "$($Script:ThePlayer.Stats[[StatId]::Speed].Base)"
+        }
+        If($Script:ThePlayer.Stats[[StatId]::Accuracy].Base -LT 10) {
+            $AccStatFmtStr = "{0:d2}" -F $Script:ThePlayer.Stats[[StatId]::Accuracy].Base
+        } Elseif($Script:ThePlayer.Stats[[StatId]::Accuracy].Base -GE 10) {
+            $AccStatFmtStr = "$($Script:ThePlayer.Stats[[StatId]::Accuracy].Base)"
+        }
+        If($Script:ThePlayer.Stats[[StatId]::Luck].Base -LT 10) {
+            $LckStatFmtStr = "{0:d2}" -F $Script:ThePlayer.Stats[[StatId]::Luck].Base
+        } Elseif($Script:ThePlayer.Stats[[StatId]::Luck].Base -GE 10) {
+            $LckStatFmtStr = "$($Script:ThePlayer.Stats[[StatId]::Luck].Base)"
+        }
+
+        $this.StatLineActual = [ATStringComposite]::new(@(
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = $Script:BATAdornmentCharTable[$Script:ThePlayer.Affinity].Item2
+                    Coordinates     = [StatusHudWindow]::StatLineDrawCoords
+                }
+                UserData = "$($Script:BATAdornmentCharTable[$Script:ThePlayer.Affinity].Item1) "
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($Script:ThePlayer.Name)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                    Coordinates     = [StatusHudWindow]::StatSeparatorDrawCoords
+                }
+                UserData = "$($AtkDispStr)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($AtkStatFmtStr) "
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($DefDispStr)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($DefStatFmtStr) "
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($MatDispStr)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($MatStatFmtStr) "
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($MdfDispStr)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($MdfStatFmtStr) "
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($SpdDispStr)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($SpdStatFmtStr) "
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundCOlor = [CCTextDefault24]::new()
+                }
+                UserData = "$($AccDispStr)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData = "$($AccStatFmtStr) "
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDeafult24]::new()
+                }
+                UserData = "$($LckDispStr)"
+            },
+            [ATString]@{
+                Prefix = [ATStringPrefix]@{
+                    ForegroundColor = [CCTextDefault24]::new()
+                }
+                UserData   = "$($LckStatFmtStr)"
+                UseATReset = $true
+            }
+        ))
+    }
+}
+
+
+
+
+
+###############################################################################
+#
 # GAME CORE
 #
 # ENTRY POINT FOR THE GAME PROGRAM
