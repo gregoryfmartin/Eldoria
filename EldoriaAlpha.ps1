d12c91aef78a759c0114bb2583ccfc75af914acd
Gregory F Martin <greg@gregfmartin.org>
2025-01-02 16:49:20 -0500

    Working

--- EldoriaAlpha.ps1
+++ EldoriaAlpha.ps1
@@ -9051,6 +9051,34 @@ Class SIInternalBase : SceneImage {
     SIInternalBase() : base() {
         $this.ColorMap = New-Object 'ATBackgroundColor24[]' ([Int32](([Int32]([SceneImage]::Width)) * ([Int32]([SceneImage]::Height))))
     }
+
+  SIInternalBase(
+    [String]$JsonConfigPath
+  ) : base() {
+    [Hashtable]$JsonData = @{}
+    $this.ColorMap = New-Object 'ATBackgroundColor24[]' ([Int32](([Int32]([SceneImage]::Width)) * ([Int32]([SceneImage]::Height))))
+
+    If($(Test-Path $JsonConfigPath) -EQ $true) {
+      $JsonData = Get-Content -Raw $JsonConfigPath | ConvertFrom-Json -AsHashtable
+
+      # THIS JSON DATA WOULD CONTAIN ONLY ONE ELEMENT CALLED COLORDATA WHICH IS A SINGLE ARRAY
+      # THAT CONTAINS EITHER A STRING, WHICH WOULD BE MAPPED TO A SPECIFIC COLOR DEFINED ABOVE,
+      # OR AN ARRAY OF R, G, B VALUES WHICH WOULD CREATE A CUSTOM COLOR.
+      [Int]$A = 0
+      Foreach($B in $JsonData['ColorData']) {
+        If($B -IS [String]) {
+          [String]$C = [String]::Format("CC{0}24", $B)
+          $this.ColorMap[$A] = New-Object "$($C)"
+        } Elseif($B -IS [Array]) {
+          $this.ColorMap = [ATBackgroundColor24]::new([ConsoleColor24]::new($B[0], $B[1], $B[2]))
+        }
+        $A++
+      }
+
+      $this.CreateSceneImageATString($this.ColorMap)
+      $this.ColorMap = $null
+    }
+  }
 }
 
 ###############################################################################
@@ -9083,7 +9111,8 @@ Class SIRandomNoise : SceneImage {
 #
 ###############################################################################
 Class SIFieldNorthRoad : SIInternalBase {
-    SIFieldNorthRoad() : base() {
+    SIFieldNorthRoad() : base("$(Get-Location)Image Data\SIFieldNorthRoad.json") {
+    <#
         # Write-Progress -Activity 'Creating Scene Images      ' -Id 3 -Status 'Creating SIFieldNorthRoad' -PercentComplete -1
         $this.ColorMap[0]   = [CCAppleBlueLight24]::new()
         $this.ColorMap[1]   = [CCAppleBlueLight24]::new()
@@ -9952,6 +9981,7 @@ Class SIFieldNorthRoad : SIInternalBase {
 
         $this.CreateSceneImageATString($this.ColorMap)
         $this.ColorMap = $null
+        #>
     }
 }
 
