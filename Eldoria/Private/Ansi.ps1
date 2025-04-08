f86401494d8a0c2cf5fc999670d5e8c32ee92ae1
Gregory F Martin <greg@gregfmartin.org>
2025-04-07 22:07:28 -0400

    Added Eld BG 24 Prefix test

--- Eldoria/Private/Ansi.ps1
+++ Eldoria/Private/Ansi.ps1
@@ -5,6 +5,10 @@ Set-StrictMode -Version Latest
 <#
 .SYNOPSIS
 Returns an ANSI SGR-formatted string to change the foreground color of any following text to a 24-bit color.
+
+.OUTPUTS
+An ANSI SGR-formatted string with FG 24-bit modifiers. Throws an Exception if ColorData contains less than 3 elements,
+any of the values in ColorData fall outside the 8-bit range (0-255), or AnsiFgCol24Prefix can't be found.
 #>
 Function New-EldFg24String {
     [CmdletBinding()]
@@ -32,5 +36,42 @@ Function New-EldFg24String {
 
     Process {
         Return "$((Get-EldVar -Name 'AnsiFgCol24Prefix').Value)$($ColorData[0]);$($ColorData[1]);$($ColorData[2])m"
+    }
+}
+
+<#
+.SYNOPSIS
+Returns an ANSI SGR-formatted string to change the background color of any following text to a 24-bit color.
+
+.OUTPUTS
+An ANSI SGR-formatted string with BG 24-bit modifiers. Throws an Exception if ColorData contains less than 3 elements,
+any of the values in ColorData fall outside the 8-bit range (0-255), or AnsiBgCol24Prefix can't be found.
+#>
+Function New-EldBg24String {
+    [CmdletBinding()]
+    Param(
+        [Int[]]$ColorData = @(0, 0, 0)
+    )
+
+    Begin {
+        If($ColorData.Length -LT 3) {
+            Throw [Exception]::new('ColorData length is less than expected.')
+        }
+
+        Foreach($Channel in $ColorData) {
+            If($Channel -LT 0 -OR $Channel -GT 255) {
+                Throw [Exception]::new('Color Channel value is out of expected range (0-255).')
+            }
+        }
+
+        Try {
+            $null = Get-EldVar -Name 'AnsiBgCol24Prefix' -ErrorAction SilentlyContinue
+        } Catch {
+            Throw $_
+        }
+    }
+
+    Process {
+        Return "$((Get-EldVar -Name 'AnsiBgCol24Prefix').Value)$($ColorData[0]);$($ColorData[1]);$($ColorData[2])m"
     }
 }
--- Eldoria/Tests/Test-Ansi.tests.ps1
+++ Eldoria/Tests/Test-Ansi.tests.ps1
@@ -42,3 +42,48 @@ Describe 'New-EldFg24String' {
         Write-Host "`e[m"
     }
 }
+
+Describe 'New-EldBg24String' {
+    BeforeAll {
+        . "$PSScriptRoot\..\Private\Ansi.ps1"
+        
+        Initialize-EldVars
+    }
+
+    It 'Produces a properly formatted ANSI SGR string with the default values' {
+        [String]$Test = New-EldBg24String
+
+        $Test | Should -BeExactly "`e[48;2;0;0;0m"
+    }
+
+    It 'Produces a properly formatted ANSI SGR string with a custom color (white)' {
+        [String]$Test = New-EldBg24String -ColorData @(255, 255, 255)
+
+        $Test | Should -BeExactly "`e[48;2;255;255;255m"
+    }
+
+    It 'Throws an exception if ColorData isn''t the proper length (3)' {
+        Try {
+            $null = New-EldBg24String -ColorData @(0, 0) | Should -Throw
+        } Catch {}
+    }
+
+    It 'Throws an exception if any of the Channel Values are out of range (0-255)' {
+        Try {
+            $null = New-EldBg24String -ColorData @(0, 0, -1) | Should -Throw
+        } Catch {}
+    }
+
+    It 'Writes an error if AnsiBgCol24Prefix isn''t set' {
+        Remove-Variable -Name 'ELD:AnsiBgCol24Prefix' -Scope Global -Force
+
+        Try {
+            $null = New-EldBg24String | Should -Throw
+        } Catch {}
+    }
+
+    AfterAll {
+        Remove-EldVars
+        Write-Host "`e[m"
+    }
+}
