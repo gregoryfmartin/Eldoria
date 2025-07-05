434cd2e9f97a58621ed1aa3fd040f233002f7c4b
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 19:32:25 -0400

    Added CCAppleVMintALight

---
+++ Classes/ConsoleColor/CCAppleVMintALight.ps1
@@ -0,0 +1,3 @@
+Class CCAppleVMintALight24 : ConsoleColor24 {
+    CCAppleVMintALight24() : base(11, 117, 112) {}
+}
---
+++ Classes/ConsoleColor/CCAppleVMintDark.ps1
@@ -0,0 +1,3 @@
+Class CCAppleVMintDark24 : ConsoleColor24 {
+    CCAppleVMintDark24() : base(108, 224, 219) {}
+}
