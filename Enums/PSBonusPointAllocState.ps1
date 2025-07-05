fb97e2a9fbd93f5d5e78305666ac6069606c4adc
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:34:11 -0400

    Added PSBPAS

---
+++ Enums/PSBonusPointAllocState.ps1
@@ -0,0 +1,17 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# PS BONUS POINT ALLOC STATE
+#
+# THE STATES THAT THE PS BONUS POINT ALLOC WINDOW GOES THROUGH.
+#
+#//////////////////////////////////////////////////////////////////////////////
+
+Enum PSBonusPointAllocState {
+    AtkPointsMod
+    DefPointsMod
+    MatPointsMod
+    MdfPointsMod
+    SpdPointsMod
+    AccPointsMod
+    LckPointsMod
+}
