d492a5d5fcb2ba2f26ae8ce2b66d8a2624030399
Gregory F Martin <greg@gregfmartin.org>
2025-07-04 23:56:12 -0400

    Added GSP

---
+++ Enums/GameStatePrimary.ps1
@@ -0,0 +1,11 @@
+Enum GameStatePrimary {
+    SplashScreenA
+    SplashScreenB
+    TitleScreen
+    PlayerSetupScreen
+    GamePlayScreen
+    InventoryScreen
+    BattleScreen
+    PlayerStatusScreen
+    Cleanup
+}
