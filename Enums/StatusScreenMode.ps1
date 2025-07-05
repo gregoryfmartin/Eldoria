5bd4a2de23c01180fe86fd61eb2f29145d61cb4b
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:23:28 -0400

    Added SSM

---
+++ Enums/StatusScreenMode.ps1
@@ -0,0 +1,18 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# STATUS SCREEN MODE
+#
+# EXPRESSES THE INTERNAL STATE OF THE STATUS SCREEN.
+#
+# EQUIPPED TECH SELECTION
+#    THE STATUS SCREEN IS SHOWING AND FOCUSED ON THE CURRENTLY EQUIPPED TECHNIQUES.
+#
+# TECH INVENTORY SELECTION
+#    THE STATUS SCREEN IS SHOWING AND FOCUSED ON THE TECH INVENTORY SUB-WINDOW.
+#
+#//////////////////////////////////////////////////////////////////////////////
+
+Enum StatusScreenMode {
+    EquippedTechSelection
+    TechInventorySelection
+}
