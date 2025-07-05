d78f31b9538d236651ea4e2ffec415b5c74ba13f
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 12:26:46 -0400

    Added CCAppleMintDark/TealLight

---
+++ Classes/ConsoleColor/CCAppleMintDark.ps1
@@ -0,0 +1,3 @@
+Class CCAppleMintDark24 : ConsoleColor24 {
+    CCAppleMintDark24() : base(99, 230, 226) {}
+}
---
+++ Classes/ConsoleColor/CCAppleTealLight.ps1
@@ -0,0 +1,3 @@
+Class CCAppleTealLight24 : ConsoleColor24 {
+    CCAppleTealLight24() : base(48, 176, 199) {}
+}
