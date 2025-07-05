8d1d849d99933c7708c254e7a05024676f0f0d45
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:27:05 -0400

    Added FCDF

---
+++ Enums/FnlCellularDistanceFunction.ps1
@@ -0,0 +1,16 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# FNL CELLULAR DISTANCE FUNCTION
+#
+# AN ENUMERATION PEGGED TO THE FAST NOISE LITE IMPLEMENTATION.
+#
+# SPECIFIES THE TYPE OF CELLULAR DISTANCE FUNCTION TO USE DURING EVALUATION.
+#
+#//////////////////////////////////////////////////////////////////////////////
+
+Enum FnlCellularDistanceFunction {
+    Euclidean
+    EuclideanSq
+    Manhattan
+    Hybrid
+}
