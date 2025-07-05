f85f7801e084f1fb3ef1d935f3e6fae674ea8ba4
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 12:19:25 -0400

    Added CCAppleOrangeLight/Dark

---
+++ Classes/ConsoleColor/CCAppleOrangeDark.ps1
@@ -0,0 +1,3 @@
+Class CCAppleOrangeDark24 : ConsoleColor24 {
+    CCAppleOrangeDark24() : base(255, 159, 10) {}
+}
---
+++ Classes/ConsoleColor/CCAppleOrangeLight.ps1
@@ -0,0 +1,3 @@
+Class CCAppleOrangeLight24 : ConsoleColor24 {
+    CCAppleOrangeLight24() : base(255, 149, 0) {}
+}
