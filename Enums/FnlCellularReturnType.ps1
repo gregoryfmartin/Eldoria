53432b004a6d68a030ef50a15ff3365270a3264b
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:28:10 -0400

    Added FCRT

---
+++ Enums/FnlCellularReturnType.ps1
@@ -0,0 +1,19 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# FNL CELLULAR RETURN TYPE
+#
+# AN ENUMERATION PEGGED TO THE FAST NOISE LITE IMPLEMENTATION.
+#
+# SPECIFIES THE TYPE OF CELLULAR RETURN WHEN EVALUATING CELL-RELATED EXPRESSIONS.
+#
+#//////////////////////////////////////////////////////////////////////////////
+
+Enum FnlCellularReturnType {
+    CellValue
+    Distance
+    Distance2
+    Distance2Add
+    Distance2Sub
+    Distance2Mul
+    Distance2Div
+}
