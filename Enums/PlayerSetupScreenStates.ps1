74cf7038b9529ae0330873681c791fdd391b5f4f
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:33:28 -0400

    Added PSSS

---
+++ Enums/PlayerSetupScreenStates.ps1
@@ -0,0 +1,17 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# PLAYER SETUP SCREEN STATES
+#
+# THE STATES THAT THE PLAYER SETUP SCREEN GOES THROUGH.
+#
+#//////////////////////////////////////////////////////////////////////////////
+
+Enum PlayerSetupScreenStates {
+    PlayerSetupSetup
+    PlayerSetupNameEntry
+    PlayerSetupGenderSelection
+    PlayerSetupPointAllocate
+    PlayerSetupAffinitySelect
+    PlayerSetupProfileSelect
+    PlayerSetupConfirmation
+}
