ab4ae15a152ee96ec483e028829d79bda1438292
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:30:03 -0400

    Added WBP

---
+++ Enums/WindowBorderPart.ps1
@@ -0,0 +1,22 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# WINDOW BORDER PART
+#
+# AN ENUMERATION INSPIRED BY SPECTRE CONSOLE AND PWSHSPECTRECONSOLE.
+# THANKS TO SHAWN LAWRIE AND TRACKD!
+#
+# SPECIFIES THE COMPONENTS OF A WINDOW'S BORDER. EACH REFERS TO A SPECIFIC CHARACTER
+# USED TO CREATE THAT PORTION.
+#
+#//////////////////////////////////////////////////////////////////////////////
+
+Enum WindowBorderPart {
+    LeftTop
+    Top
+    RightTop
+    Right
+    RightBottom
+    Bottom
+    LeftBottom
+    Left
+}
