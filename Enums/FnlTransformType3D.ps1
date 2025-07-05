67a76a99758ae8b7a0c04ea43e8fa45977a2f9c5
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:29:28 -0400

    Added FTT3D

---
+++ Enums/FnlTransformType3D.ps1
@@ -0,0 +1,16 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# FNL TRANSFORM TYPE 3D
+#
+# AN ENUMERATION PEGGED TO THE FAST NOISE LITE IMPLEMENTATION.
+#
+# SPECIFIES THE TYPE OF 3D TRANSFORM TO USE DURING EVALUATION.
+#
+#//////////////////////////////////////////////////////////////////////////////
+
+Enum FnlTransformType3D {
+    None
+    ImproveXYPlanes
+    ImproveXZPlanes
+    DefaultOpenSimplex2
+}
