3fabc44c5ad2c03823c74c17d16de03270d5c5f2
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 05:44:43 -0400

    Added ConsoleColor24

---
+++ Classes/ConsoleColor24.ps1
@@ -0,0 +1,15 @@
+Class ConsoleColor24 {
+    [ValidateRange(0, 255)][Int]$Red
+    [ValidateRange(0, 255)][Int]$Green
+    [ValidateRange(0, 255)][Int]$Blue
+
+    ConsoleColor24(
+        [Int]$Red,
+        [Int]$Green,
+        [Int]$Blue
+    ) {
+        $this.Red   = $Red
+        $this.Green = $Green
+        $this.Blue  = $Blue
+    }
+}
