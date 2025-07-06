1f6ba29c339d7a0f65fe4486bb216253afd7d9f2
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 09:51:50 -0400

    Added BATempest

---
+++ Classes/CombatEnginePrimitives/BattleActions/BATempest.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BATempest : BattleAction {
+    BATempest() : base() {
+        $this.Name        = 'Tempest'
+        $this.Description = 'If it were a tempest of love, would you feel any different?'
+        $this.Type        = [BattleActionType]::ElementalWater
+        $this.MpCost      = 400
+        $this.EffectValue = 2000
+        $this.Chance      = 0.1
+    }
+}
