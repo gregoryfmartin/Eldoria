769d933a04780f56087aa6ec218abdd2bebc6fe0
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:41:06 -0400

    Added BABrilliance

---
+++ Classes/CombatEnginePrimitives/BattleActions/BABrilliance.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BABrilliance : BattleAction { 
+    BABrilliance() : base() {
+        $this.Name        = 'Brilliance'
+        $this.Description = 'How I feel when I look at myself in the mirror.'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 300
+        $this.EffectValue = 1500
+        $this.Chance      = 0.1
+    }
+}
