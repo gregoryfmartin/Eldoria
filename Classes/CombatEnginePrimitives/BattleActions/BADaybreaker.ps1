80002a55371970d93609db41d1e0ea268931807c
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:40:22 -0400

    Added BADaybreaker

---
+++ Classes/CombatEnginePrimitives/BattleActions/BADaybreaker.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BADaybreaker : BattleAction {
+    BADaybreaker() : base() {
+        $this.Name        = 'Daybreaker'
+        $this.Description = 'Some statue in Skyrim gave me this.'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 300
+        $this.EffectValue = 1500
+        $this.Chance      = 0.1
+    }
+}
