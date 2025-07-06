eff85d334c66c063a0d74f9a8fb1a62ff3002ff2
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:35:58 -0400

    Added BALeafShield

---
+++ Classes/CombatEnginePrimitives/BattleActions/BALeafShield.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BALeafShield : BattleAction {
+    BALeafShield() : base() {
+        $this.Name        = 'Leaf Shield'
+        $this.Description = 'Are you sure this''ll work?'
+        $this.Type        = [BattleActionType]::ElementalWind
+        $this.MpCost      = 200
+        $this.EffectValue = 1000
+        $this.Chance      = 0.1
+    }
+}
