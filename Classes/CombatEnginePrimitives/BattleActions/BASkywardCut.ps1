daced4b9c54341a826543c0f9f5a0eafcea7fc56
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:32:18 -0400

    Added BASkywardCut

---
+++ Classes/CombatEnginePrimitives/BattleActions/BASkywardCut.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BASkywardCut : BattleAction {
+    BASkywardCut() : base() {
+        $this.Name        = 'Skyward Cut'
+        $this.Description = 'Remember to always cut away from yourself.'
+        $this.Type        = [BattleActionType]::ElementalWind
+        $this.MpCost      = 100
+        $this.EffectValue = 500
+        $this.Chance      = 0.1
+    }
+}
