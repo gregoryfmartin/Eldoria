985f8d1c5b359443c7a485ccb52419e409caa38a
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 21:50:06 -0400

    Added BASwordStab

---
+++ Classes/CombatEnginePrimitives/BattleActions/BASwordStab.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BASwordStab : BattleAction {
+    BASwordStab() : base() {
+        $this.Name        = 'Sword Stab'
+        $this.Description = 'This was practiced with toothpicks.'
+        $this.Type        = [BattleActionType]::Physical
+        $this.MpCost      = 0
+        $this.EffectValue = 80
+        $this.Chance      = 0.7
+    }
+}
