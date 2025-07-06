2bcf9c438dba72829b0cfd983b976506ad528b2b
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 23:01:51 -0400

    Added BAIcicleStrike

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAIcicleStrike.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAIcicleStrike : BattleAction {
+    BAIcicleStrike() : base() {
+        $this.Name        = 'Icicle Strike'
+        $this.Description = 'When they''re this big, who needs a sword?'
+        $this.Type        = [BattleActionType]::ElementalIce
+        $this.MpCost      = 5
+        $this.EffectValue = 80
+        $this.Chance      = 0.9
+    }
+}
