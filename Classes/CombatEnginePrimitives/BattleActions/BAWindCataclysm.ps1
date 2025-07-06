61721b188de83edf577a837a3f3bbbe0ac9f0c66
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:36:35 -0400

    Added BAWindCataclysm

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAWindCataclysm.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAWindCataclysm : BattleAction {
+    BAWindCataclysm() : base() {
+        $this.Name        = "`u{03C6} Cataclysm"
+        $this.Description = 'Windy death rains down upon you.'
+        $this.Type        = [BattleActionType]::ElementalWind
+        $this.MpCost      = 300
+        $this.EffectValue = 1500
+        $this.Chance      = 0.1
+    }
+}
