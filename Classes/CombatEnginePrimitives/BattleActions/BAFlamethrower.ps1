da42fdec6c764c88e21b145806906208928572c5
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 22:14:41 -0400

    Added BAFlamethrower

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAFlamethrower.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAFlamethrower : BattleAction {
+    BAFlamethrower() : base() {
+        $this.Name        = 'Flamethrower'
+        $this.Description = 'Our inspiration was Elon.'
+        $this.Type        = [BattleActionType]::ElementalFire
+        $this.MpCost      = 10
+        $this.EffectValue = 90
+        $this.Chance      = 0.7
+    }
+}
