3e3fc81009d2e9db93f21c4d473779cb71cfed4f
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:40:45 -0400

    Added BAAngelicHymn

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAAngelicHymn.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAAngelicHymn : BattleAction {
+    BAAngelicHymn() : base() {
+        $this.Name        = 'Angelic Hymn'
+        $this.Description = 'This is how I sound when I sing Britney Spears.'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 300
+        $this.EffectValue = 1500
+        $this.Chance      = 0.1
+    }
+}
