c4f00482ca26e9023d9a5f70de16b68911074232
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:38:40 -0400

    Added BAPrisimShock

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAPrisimShock.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAPrismShock : BattleAction {
+    BAPrismShock() : base() {
+        $this.Name        = 'Prism Shock'
+        $this.Description = 'The pretty rainbow of death.'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 300
+        $this.EffectValue = 1500
+        $this.Chance      = 0.1
+    }
+}
