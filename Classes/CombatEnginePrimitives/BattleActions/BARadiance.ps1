59ea5cbb9265f02f01af991549d3570b751d1d8f
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:37:42 -0400

    Added BARadiance

---
+++ Classes/CombatEnginePrimitives/BattleActions/BARadiance.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BARadiance : BattleAction {
+    BARadiance() : base() {
+        $this.Name        = 'Radiance'
+        $this.Description = 'All teh brights.'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 100
+        $this.EffectValue = 500
+        $this.Chance      = 0.1
+    }
+}
