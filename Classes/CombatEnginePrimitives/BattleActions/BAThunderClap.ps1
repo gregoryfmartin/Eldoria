fae4ec0f3ea93f13eb640bd0db2ed59e664194de
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:31:54 -0400

    Added BAThunderClap

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAThunderClap.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAThunderClap : BattleAction {
+    BAThunderClap() : base() {
+        $this.Name        = 'Thunder Clap'
+        $this.Description = 'Sometimes an euphemism, this time a threat.'
+        $this.Type        = [BattleActionType]::ElementalWind
+        $this.MpCost      = 100
+        $this.EffectValue = 500
+        $this.Chance      = 0.1
+    }
+}
