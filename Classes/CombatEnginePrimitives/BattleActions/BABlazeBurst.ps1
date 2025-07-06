3564e55d9cd9e67c7f18f9883e20ad916d1de96a
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 22:13:11 -0400

    Added BABlazeBurst

---
+++ Classes/CombatEnginePrimitives/BattleActions/BABlazeBurst.ps1
@@ -0,0 +1,14 @@
+using namspace System
+
+Set-StrictMode -Version Latest
+
+Class BABlazeBurst : BattleAction {
+    BABlazeBurst() : base() {
+        $this.Name        = 'Blaze Burst'
+        $this.Description = 'Like an arc flash, only worse.'
+        $this.Type        = [BattleActionType]::ElementalFire
+        $this.MpCost      = 10
+        $this.EffectValue = 80
+        $this.Chance      = 0.8
+    }
+}
