366bcceaf491d01282de8eed4ca3319379b11d4c
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 23:15:04 -0400

    Added BATidalCrush

---
+++ Classes/CombatEnginePrimitives/BattleActions/BATidalCrush.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BATidalCrush : BattleAction {
+    BATidalCrush() : base() {
+        $this.Name        = 'Tidal Crush'
+        $this.Description = 'Your high school crush came to kill you, in water form.'
+        $this.Type        = [BattleActionType]::ElementalWater
+        $this.MpCost      = 50
+        $this.EffectValue = 250
+        $this.Chance      = 0.5
+    }
+}
