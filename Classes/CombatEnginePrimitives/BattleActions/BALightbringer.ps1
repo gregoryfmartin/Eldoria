31f97321d6069157d62a4770fc81029e192c0624
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:39:19 -0400

    Added BALightbringer

---
+++ Classes/CombatEnginePrimitives/BattleActions/BALightbringer.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BALightbringer : BattleAction {
+    BALightbringer() : base() {
+        $this.Name        = 'Lightbringer'
+        $this.Description = 'Bring the party!'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 300
+        $this.EffectValue = 1500
+        $this.Chance      = 0.1
+    }
+}
