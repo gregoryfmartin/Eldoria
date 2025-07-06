4b528a0ff7dfe71392d225d3cbb2f6db36656ccf
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:43:01 -0400

    Added BASunfire

---
+++ Classes/CombatEnginePrimitives/BattleActions/BASunfire.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BASunfire : BattleAction {
+    BASunfire() : base() {
+        $this.Name        = 'Sunfire'
+        $this.Description = 'Scorched Earth, mofo.'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 400
+        $this.EffectValue = 2000
+        $this.Chance      = 0.1
+    }
+}
