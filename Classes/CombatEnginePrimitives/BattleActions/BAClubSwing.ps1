26b9843f698746c9a37c0590c464e2645c1fd1c4
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 22:03:05 -0400

    Added BAClubSwing

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAClubSwing.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAClubSwing : BattleAction {
+    BAClubSwing() : base() {
+        $this.Name        = 'Club Swing'
+        $this.Description = 'Me Ooga. Me swing-um big-um stick.'
+        $this.Type        = [BattleActionType]::Physical
+        $this.MpCost      = 0
+        $this.EffectValue = 70
+        $this.Chance      = 0.7
+    }
+}
