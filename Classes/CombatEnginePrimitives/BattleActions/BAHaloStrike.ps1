e718ab66a77e43ce478dec56f80e848159cc7f94
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:38:59 -0400

    Added BAHaloStrike

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAHaloStrike.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAHaloStrike : BattleAction {
+    BAHaloStrike() : base() {
+        $this.Name        = 'Halo Strike'
+        $this.Description = 'These surprisingly hurt.'
+        $this.Type        = [BattleActionType]::ElementalLight
+        $this.MpCost      = 300
+        $this.EffectValue = 1500
+        $this.Chance      = 0.1
+    }
+}
