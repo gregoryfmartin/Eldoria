9a60530c3f2a8e407bda5ccd2efd82a7e430fcda
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 10:30:33 -0400

    Added BAGaleStrike

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAGaleStrike.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAGaleStrike : BattleAction {
+    BAGaleStrike() : base() {
+        $this.Name        = 'Gale Strike'
+        $this.Description = 'The wind can hurt you.'
+        $this.Type        = [BattleActionType]::ElementalWind
+        $this.MpCost      = 50
+        $this.EffectValue = 250
+        $this.Chance      = 0.5
+    }
+}
