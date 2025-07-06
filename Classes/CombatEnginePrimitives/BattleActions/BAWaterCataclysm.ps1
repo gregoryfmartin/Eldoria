3a46a3212cf5ed83e1649e77a1e9e3a1147f9d3a
Gregory F Martin <greg@gregfmartin.org>
2025-07-06 09:53:45 -0400

    Added BAWaterCataclysm

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAWaterCataclysm.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAWaterCataclysm : BattleAction {
+    BAWaterCataclysm() : base() {
+        $this.Name        = "`u{03C8} Cataclysm"
+        $this.Description = 'Watery death rains down upon you.'
+        $this.Type        = [BattleActionType]::ElementalWater
+        $this.MpCost      = 400
+        $this.EffectValue = 2000
+        $this.Chance      = 0.1
+    }
+}
