46b9760c404a4594bd5a6ae54ecf8e457fb3247a
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 23:11:42 -0400

    Added BAWaterWhip

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAWaterWhip.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAWaterWhip : BattleAction {
+    BAWaterWhip() : base() {
+        $this.Name        = 'Water Whip'
+        $this.Description = 'Indiana Jones''s least favorite whip.'
+        $this.Type        = [BattleActionType]::ElementalWater
+        $this.MpCost      = 5
+        $this.EffectValue = 80
+        $this.Chance      = 0.9
+    }
+}
