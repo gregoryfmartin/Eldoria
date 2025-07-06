1684d344f6fe39d06ecb7b2c139ae639455dec74
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 23:05:04 -0400

    Added BAFrostWave

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAFrostWave.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAFrostWave : BattleAction {
+    BAFrostWave() : base() {
+        $this.Name        = 'Frost Wave'
+        $this.Description = 'Ride the wave, dude.'
+        $this.Type        = [BattleActionType]::ElementalIce
+        $this.MpCost      = 15
+        $this.EffectValue = 100
+        $this.Chance      = 1.0
+    }
+}
