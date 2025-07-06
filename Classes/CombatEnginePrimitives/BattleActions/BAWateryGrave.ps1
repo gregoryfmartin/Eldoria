f854cd5e02f7f2cb7e89bda4d8c9aec1b87667a0
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 23:22:42 -0400

    Added BAWateryGrave

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAWateryGrave.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAWateryGrave : BattleAction {
+    BAWateryGrave() : base() {
+        $this.Name        = 'Watery Grave'
+        $this.Description = 'Davey Jones is holed up here.'
+        $this.Type        = [BattleActionType]::ElementalWater
+        $this.MpCost      = 200
+        $this.EffectValue = 1000
+        $this.Chance      = 0.1
+    }
+}
