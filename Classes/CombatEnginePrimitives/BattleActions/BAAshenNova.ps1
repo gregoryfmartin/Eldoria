2b56a9893229dc3f944a6143b46b284362728a56
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 22:16:46 -0400

    Added BAAshenNova

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAAshenNova.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAAshenNova : BattleAction {
+    BAAshenNova() : base() {
+        $this.Name        = 'Ashen Nova'
+        $this.Description = 'Reminds me of Pompeii. Only worse.'
+        $this.Type        = [BattleActionType]::ElementalFire
+        $this.MpCost      = 50
+        $this.EffectValue = 250
+        $this.Chance      = 0.5
+    }
+}
