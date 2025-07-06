f32e2e4df5a3c270b4e040a40dac796ceedcbd39
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 23:17:53 -0400

    Added BATyphoon

---
+++ Classes/CombatEnginePrimitives/BattleActions/BATyphoon.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BATyphoon : BattleAction {
+    BATyphoon() : base() {
+        $this.Name        = 'Typhoon'
+        $this.Description = 'Not to be confused with the infamous Tie Foon.'
+        $this.Type        = [BattleActionType]::ElementalWater
+        $this.MpCost      = 200
+        $this.EffectValue = 1000
+        $this.Chance      = 0.1
+    }
+}
