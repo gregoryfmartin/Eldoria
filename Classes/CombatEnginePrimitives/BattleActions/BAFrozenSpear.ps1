0772dcc09944d4bb14ec7b6ebe4ba6fc6c849768
Gregory F Martin <greg@gregfmartin.org>
2025-07-05 23:07:01 -0400

    Added BAHailstorm

---
+++ Classes/CombatEnginePrimitives/BattleActions/BAFrozenSpear.ps1
@@ -0,0 +1,14 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Class BAFrozenSpear : BattleAction {
+    BAFrozenSpear() : base() {
+        $this.Name        = 'Frozen Spear'
+        $this.Description = 'I found this spear in a fridge.'
+        $this.Type        = [BattleActionType]::ElementalIce
+        $this.MpCost      = 15
+        $this.EffectValue = 100
+        $this.Chance      = 1.0
+    }
+}
