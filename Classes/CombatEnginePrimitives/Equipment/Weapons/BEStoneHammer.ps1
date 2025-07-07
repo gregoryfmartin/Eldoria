d40f400ea7bb828340f6f0c8b8b0d00386b9c4ff
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:49:12 -0400

    Added BEStoneHammer

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEStoneHammer.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE STONE HAMMER
+#
+###############################################################################
+
+Class BEStoneHammer : BEWeapon {
+	BEStoneHammer() : base() {
+		$this.Name          = 'Stone Hammer'
+		$this.MapObjName    = 'stonehammer'
+		$this.PurchasePrice = 70
+		$this.SellPrice     = 35
+		$this.TargetStats   = @{
+			[StatId]::Attack = 9
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A primitive hammer with a stone head.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
