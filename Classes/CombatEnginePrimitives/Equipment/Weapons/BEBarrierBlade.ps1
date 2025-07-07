56d92dfcf85ab0bcd68b57857dcb53cc0cc06d74
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 14:18:47 -0400

    Added BEBarrierBlade

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEBarrierBlade.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BARRIER BLADE
+#
+###############################################################################
+
+Class BEBarrierBlade : BEWeapon {
+	BEBarrierBlade() : base() {
+		$this.Name          = 'Barrier Blade'
+		$this.MapObjName    = 'barrierblade'
+		$this.PurchasePrice = 850
+		$this.SellPrice     = 425
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 42
+			[StatId]::MagicAttack = 12
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sword that can temporarily create a magical shield.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
