2ad8086fcf415c24bfd0be7f1183ad20ed103df2
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 14:13:05 -0400

    Added BEElvenBow

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEElvenBow.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ELVEN BOW
+#
+###############################################################################
+
+Class BEElvenBow : BEWeapon {
+	BEElvenBow() : base() {
+		$this.Name          = 'Elven Bow'
+		$this.MapObjName    = 'elvenbow'
+		$this.PurchasePrice = 1200
+		$this.SellPrice     = 600
+		$this.TargetStats   = @{
+			[StatId]::Attack = 62
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A gracefully crafted bow, known for its incredible accuracy.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
