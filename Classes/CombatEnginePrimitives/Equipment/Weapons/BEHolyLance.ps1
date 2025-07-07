9682d595bc74251faa55ca589c55a3c5ca7b6558
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 14:12:16 -0400

    Added BEHolyLance

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEHolyLance.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HOLY LANCE
+#
+###############################################################################
+
+Class BEHolyLance : BEWeapon {
+	BEHolyLance() : base() {
+		$this.Name          = 'Holy Lance'
+		$this.MapObjName    = 'holylance'
+		$this.PurchasePrice = 950
+		$this.SellPrice     = 475
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 50
+			[StatId]::MagicAttack = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A blessed spear said to smite evil.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
