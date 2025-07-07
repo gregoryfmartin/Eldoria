06f54e0def6425b5724182069f1845c0f75ba360
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:39:18 -0400

    Added BEWarHammer

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEWarHammer.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE WAR HAMMER
+#
+###############################################################################
+
+Class BEWarHammer : BEWeapon {
+	BEWarHammer() : base() {
+		$this.Name          = 'War Hammer'
+		$this.MapObjName    = 'warhammer'
+		$this.PurchasePrice = 300
+		$this.SellPrice     = 150
+		$this.TargetStats   = @{
+			[StatId]::Attack = 23
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A heavy, blunt weapon designed to crush armor.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
