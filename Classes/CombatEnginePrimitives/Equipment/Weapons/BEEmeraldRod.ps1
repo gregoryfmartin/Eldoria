d105fdf94f4ba5e47c8d4e9aa52c6a9fb4c8e73f
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 15:33:19 -0400

    Added BEEmeraldRod

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEEmeraldRod.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE EMERALD ROD
+#
+###############################################################################
+
+Class BEEmeraldRod : BEWeapon {
+	BEEmeraldRod() : base() {
+		$this.Name          = 'Emerald Rod'
+		$this.MapObjName    = 'emeraldrod'
+		$this.PurchasePrice = 750
+		$this.SellPrice     = 375
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 10
+			[StatId]::MagicAttack = 44
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A rod adorned with a shimmering emerald, boosting earth magic.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
