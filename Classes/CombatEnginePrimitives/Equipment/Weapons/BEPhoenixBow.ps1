0829992cafd442f490ca1da697f4eeecf0149c09
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 15:37:52 -0400

    Added BEPhoenixBow

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEPhoenixBow.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE PHOENIX BOW
+#
+###############################################################################
+
+Class BEPhoenixBow : BEWeapon {
+	BEPhoenixBow() : base() {
+		$this.Name          = 'Phoenix Bow'
+		$this.MapObjName    = 'phoenixbow'
+		$this.PurchasePrice = 1250
+		$this.SellPrice     = 625
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 63
+			[StatId]::MagicAttack = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A bow adorned with phoenix feathers, allowing arrows to ignite upon impact.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
