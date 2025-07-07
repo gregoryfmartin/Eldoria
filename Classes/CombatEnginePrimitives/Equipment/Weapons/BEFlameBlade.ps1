ae1ff7425c9ab12e6181741c38eedc3c96282a91
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 14:15:08 -0400

    Added BEFlameBlade

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEFlameBlade.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE FLAME BLADE
+#
+###############################################################################
+
+Class BEFlameBlade : BEWeapon {
+	BEFlameBlade() : base() {
+		$this.Name          = 'Flame Blade'
+		$this.MapObjName    = 'flameblade'
+		$this.PurchasePrice = 780
+		$this.SellPrice     = 390
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 48
+			[StatId]::MagicAttack = 8
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sword wreathed in fire, dealing burn damage.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
