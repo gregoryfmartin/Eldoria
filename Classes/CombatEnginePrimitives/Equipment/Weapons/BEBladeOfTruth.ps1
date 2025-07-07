4443fbba13d730577c46fc428af6f4f26d89d72a
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 14:21:09 -0400

    Added BEBladeOfTruth

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEBladeOfTruth.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BLADE OF TRUTH
+#
+###############################################################################
+
+Class BEBladeOfTruth : BEWeapon {
+	BEBladeOfTruth() : base() {
+		$this.Name          = 'Blade of Truth'
+		$this.MapObjName    = 'bladeoftruth'
+		$this.PurchasePrice = 1150
+		$this.SellPrice     = 575
+		$this.TargetStats   = @{
+			[StatId]::Attack = 60
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A legendary sword said to reveal hidden weaknesses.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
