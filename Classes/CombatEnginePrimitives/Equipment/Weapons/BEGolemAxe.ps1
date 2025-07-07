7a8d6fb7fad2d2a46de09056b8efc93a513acf29
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 15:38:20 -0400

    Added BEGolemAxe

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEGolemAxe.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GOLEM AXE
+#
+###############################################################################
+
+Class BEGolemAxe : BEWeapon {
+	BEGolemAxe() : base() {
+		$this.Name          = 'Golem Axe'
+		$this.MapObjName    = 'golemaxe'
+		$this.PurchasePrice = 1020
+		$this.SellPrice     = 510
+		$this.TargetStats   = @{
+			[StatId]::Attack = 61
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'An axe designed to shatter stone and metal, incredibly heavy.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
