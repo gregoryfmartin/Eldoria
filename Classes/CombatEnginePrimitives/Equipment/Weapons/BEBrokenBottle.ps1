daaa0da01a6af029091c82b35140071c97b100be
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:59:59 -0400

    Added BEBrokenBottle

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEBrokenBottle.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BROKEN BOTTLE
+#
+###############################################################################
+
+Class BEBrokenBottle : BEWeapon {
+	BEBrokenBottle() : base() {
+		$this.Name          = 'Broken Bottle'
+		$this.MapObjName    = 'brokenbottle'
+		$this.PurchasePrice = 25
+		$this.SellPrice     = 12
+		$this.TargetStats   = @{
+			[StatId]::Attack = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A shattered bottle. Very sharp, but fragile.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
