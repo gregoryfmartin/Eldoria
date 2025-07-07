cfd412c520a6d85fcba9474aff8ba40341936525
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 15:32:45 -0400

    Added BEDarknessBow

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEDarknessBow.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DARKNESS BOW
+#
+###############################################################################
+
+Class BEDarknessBow : BEWeapon {
+	BEDarknessBow() : base() {
+		$this.Name          = 'Darkness Bow'
+		$this.MapObjName    = 'darknessbow'
+		$this.PurchasePrice = 980
+		$this.SellPrice     = 490
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 52
+			[StatId]::MagicAttack = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A bow that fires arrows of pure shadow, obscuring vision.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
