f371846d42f8ca61979b9c9891049349d48a9fe3
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:36:31 -0400

    Added BEIronSword

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEIronSword.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE IRON SWORD
+#
+###############################################################################
+
+Class BEIronSword : BEWeapon {
+	BEIronSword() : base() {
+		$this.Name          = 'Iron Sword'
+		$this.MapObjName    = 'ironsword'
+		$this.PurchasePrice = 280
+		$this.SellPrice     = 140
+		$this.TargetStats   = @{
+			[StatId]::Attack = 20
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A well-forged iron sword, more durable than bronze.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
