3d511061330330aeeaacf10f040951d13ee957db
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:28:54 -0400

    Added BEHuntingKnife

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEHuntingKnife.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HUNTING KNIFE
+#
+###############################################################################
+
+Class BEHuntingKnife : BEWeapon {
+	BEHuntingKnife() : base() {
+		$this.Name          = 'Hunting Knife'
+		$this.MapObjName    = 'huntingknife'
+		$this.PurchasePrice = 65
+		$this.SellPrice     = 32
+		$this.TargetStats   = @{
+			[StatId]::Attack = 7
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A versatile knife for hunting and survival.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
