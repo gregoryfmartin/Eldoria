423aa50db9e898ccf09f3e212644ecd6a362135e
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 14:13:30 -0400

    Added BEScourgeWhip

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEScourgeWhip.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SCOURGE WHIP
+#
+###############################################################################
+
+Class BEScourgeWhip : BEWeapon {
+	BEScourgeWhip() : base() {
+		$this.Name          = 'Scourge Whip'
+		$this.MapObjName    = 'scourgewhip'
+		$this.PurchasePrice = 700
+		$this.SellPrice     = 350
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 35
+			[StatId]::MagicAttack = 20
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A whip enchanted with dark magic, draining foes'' vitality.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
