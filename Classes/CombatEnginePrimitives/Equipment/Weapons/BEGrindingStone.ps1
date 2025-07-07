34ef96f880d17fd1b8cedec63d9f8bd524edc140
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:39:45 -0400

    Added BEGrindingStone

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEGrindingStone.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GRINDING STONE
+#
+###############################################################################
+
+Class BEGrindingStone : BEWeapon {
+	BEGrindingStone() : base() {
+		$this.Name          = 'Grinding Stone'
+		$this.MapObjName    = 'grindingstone'
+		$this.PurchasePrice = 50
+		$this.SellPrice     = 25
+		$this.TargetStats   = @{
+			[StatId]::Attack = 7
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A heavy stone used for grinding. Surprisingly effective.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
