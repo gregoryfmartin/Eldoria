0d747dc027afe9d18258a35ed555c4e3102f0ee2
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:32:30 -0400

    Added BESmallShield

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BESmallShield.ps1
@@ -0,0 +1,24 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SMALL SHIELD
+#
+###############################################################################
+
+Class BESmallShield : BEWeapon {
+	BESmallShield() : base() {
+		$this.Name          = 'Small Shield'
+		$this.MapObjName    = 'smallshield'
+		$this.PurchasePrice = 70
+		$this.SellPrice     = 35
+		$this.TargetStats   = @{
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A very small shield, offers minimal protection.'
+		$this.PlayerEffectString = ""
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
