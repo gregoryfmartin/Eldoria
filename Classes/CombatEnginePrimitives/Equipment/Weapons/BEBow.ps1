3ac3658455a58262ac115a8e590389556aa1aa30
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:23:46 -0400

    Added BEBow

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEBow.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BOW
+#
+###############################################################################
+
+Class BEBow : BEWeapon {
+	BEBow() : base() {
+		$this.Name          = 'Bow'
+		$this.MapObjName    = 'bow'
+		$this.PurchasePrice = 180
+		$this.SellPrice     = 90
+		$this.TargetStats   = @{
+			[StatId]::Attack = 14
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A basic wooden bow, requires arrows.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
