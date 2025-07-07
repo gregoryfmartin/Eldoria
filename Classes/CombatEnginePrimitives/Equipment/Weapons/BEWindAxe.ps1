8306b04fd66b1ce2d5318088795dd0b833924a46
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 15:31:32 -0400

    Added BEWindAxe

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEWindAxe.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE WIND AXE
+#
+###############################################################################
+
+Class BEWindAxe : BEWeapon {
+	BEWindAxe() : base() {
+		$this.Name          = 'Wind Axe'
+		$this.MapObjName    = 'windaxe'
+		$this.PurchasePrice = 920
+		$this.SellPrice     = 460
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 54
+			[StatId]::MagicAttack = 5
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'An axe that cuts through the air with ease, creating gusts of wind.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
