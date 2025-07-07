dc6ac2dd63b4880310d68c21d1d63c91d967b249
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 15:31:57 -0400

    Added BERuneStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BERuneStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE RUNE STAFF
+#
+###############################################################################
+
+Class BERuneStaff : BEWeapon {
+	BERuneStaff() : base() {
+		$this.Name          = 'Rune Staff'
+		$this.MapObjName    = 'runestaff'
+		$this.PurchasePrice = 880
+		$this.SellPrice     = 440
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 8
+			[StatId]::MagicAttack = 52
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff inscribed with ancient runes, enhancing spellcasting.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
