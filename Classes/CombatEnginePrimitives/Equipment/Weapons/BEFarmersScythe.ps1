4854b79d01c5abe935bcbd2b0a005279b658cee0
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:59:34 -0400

    Added BEFarmersScythe

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEFarmersScythe.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE FARMERS SCYTHE
+#
+###############################################################################
+
+Class BEFarmersScythe : BEWeapon {
+	BEFarmersScythe() : base() {
+		$this.Name          = 'Farmer''s Scythe'
+		$this.MapObjName    = 'farmersscythe'
+		$this.PurchasePrice = 160
+		$this.SellPrice     = 80
+		$this.TargetStats   = @{
+			[StatId]::Attack = 16
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A tool for harvesting crops, dangerous in combat.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
