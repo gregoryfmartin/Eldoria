d580987925a626d8a93004d832ac9dd83c235f30
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 12:20:21 -0400

    Added BEIronAxe

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEIronAxe.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE IRON AXE
+#
+###############################################################################
+
+Class BEIronAxe : BEWeapon {
+	BEIronAxe() : base() {
+		$this.Name          = 'Iron Axe'
+		$this.MapObjName    = 'ironaxe'
+		$this.PurchasePrice = 150
+		$this.SellPrice     = 75
+		$this.TargetStats   = @{
+			[StatId]::Attack = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A heavy iron axe, effective against armored foes.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
