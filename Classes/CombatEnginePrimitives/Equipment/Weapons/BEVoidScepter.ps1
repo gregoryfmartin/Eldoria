1a4338b74e71ec29c663b783ae70495c819718b5
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 15:06:23 -0400

    Added BEVoidScepter

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEVoidScepter.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE VOID SCEPTER
+#
+###############################################################################
+
+Class BEVoidScepter : BEWeapon {
+	BEVoidScepter() : base() {
+		$this.Name          = 'Void Scepter'
+		$this.MapObjName    = 'voidscepter'
+		$this.PurchasePrice = 1000
+		$this.SellPrice     = 500
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 15
+			[StatId]::MagicAttack = 60
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A scepter that can manipulate spatial anomalies.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
