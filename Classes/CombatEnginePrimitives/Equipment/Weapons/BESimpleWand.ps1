dbbcde5bcadad7158b8b4597eeff9d1521b7049d
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 13:41:19 -0400

    Added BESimpleWand

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BESimpleWand.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SIMPLE WAND
+#
+###############################################################################
+
+Class BESimpleWand : BEWeapon {
+	BESimpleWand() : base() {
+		$this.Name          = 'Simple Wand'
+		$this.MapObjName    = 'simplewand'
+		$this.PurchasePrice = 150
+		$this.SellPrice     = 75
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 3
+			[StatId]::MagicAttack = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A basic wand for casting simple spells.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
