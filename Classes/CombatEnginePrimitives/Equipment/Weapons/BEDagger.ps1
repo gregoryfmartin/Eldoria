840cd78e7775e2baba51ec3ed5477dcef9989242
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 12:17:08 -0400

    Added BEDagger

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEDagger.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DAGGER
+#
+###############################################################################
+
+Class BEDagger : BEWeapon {
+	BEDagger() : base() {
+		$this.Name          = 'Dagger'
+		$this.MapObjName    = 'dagger'
+		$this.PurchasePrice = 75
+		$this.SellPrice     = 37
+		$this.TargetStats   = @{
+			[StatId]::Attack = 8
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A small, sharp blade for quick attacks.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
