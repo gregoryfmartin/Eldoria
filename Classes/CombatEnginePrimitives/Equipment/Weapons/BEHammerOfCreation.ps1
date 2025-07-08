0197d2b034243d44b9ef7994c5c5fbad561f5e08
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:07:59 -0400

    Added BEHammerOfCreation

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEHammerOfCreation.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HAMMER OF CREATION
+#
+###############################################################################
+
+Class BEHammerOfCreation : BEWeapon {
+	BEHammerOfCreation() : base() {
+		$this.Name          = 'Hammer of Creation'
+		$this.MapObjName    = 'hammerofcreation'
+		$this.PurchasePrice = 6900
+		$this.SellPrice     = 3450
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 180
+			[StatId]::MagicAttack = 90
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A hammer said to have helped shape the world, capable of mending.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
