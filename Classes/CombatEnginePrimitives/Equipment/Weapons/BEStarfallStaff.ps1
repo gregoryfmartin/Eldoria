9a7d1e0d6284c35d5e9adfb293f53f615affd3e3
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:58:11 -0400

    Added BEStarfallStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEStarfallStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE STARFALL STAFF
+#
+###############################################################################
+
+Class BEStarfallStaff : BEWeapon {
+	BEStarfallStaff() : base() {
+		$this.Name          = 'Starfall Staff'
+		$this.MapObjName    = 'starfallstaff'
+		$this.PurchasePrice = 4800
+		$this.SellPrice     = 2400
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 22
+			[StatId]::MagicAttack = 125
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff that can call down small celestial bodies, dealing area damage.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
