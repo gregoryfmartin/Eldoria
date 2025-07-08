076ff8c90f13a1762281cdefa15cb77b1ae801aa
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:07:28 -0400

    Added BEWintersChillStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEWintersChillStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE WINTERS CHILL STAFF
+#
+###############################################################################
+
+Class BEWintersChillStaff : BEWeapon {
+	BEWintersChillStaff() : base() {
+		$this.Name          = 'Winter''s Chill Staff'
+		$this.MapObjName    = 'winterschillstaff'
+		$this.PurchasePrice = 6000
+		$this.SellPrice     = 3000
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 42
+			[StatId]::MagicAttack = 165
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff that freezes anything it touches, creating icy blasts.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
