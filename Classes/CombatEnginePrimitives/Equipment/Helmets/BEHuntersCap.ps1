3ffe2e3b516c9ccc5a5d9aae7c91d1e500285c76
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 11:39:02 -0400

    Added BEHuntersCAp

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEHuntersCap.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HUNTERS CAP
+#
+###############################################################################
+
+Class BEHuntersCap : BEHelmet {
+	BEHuntersCap() : base() {
+		$this.Name               = 'Hunter''s Cap'
+		$this.MapObjName         = 'hunterscap'
+		$this.PurchasePrice      = 110
+		$this.SellPrice          = 55
+		$this.TargetStats        = @{
+			[StatId]::Defense = 4
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A practical cap for hunters, designed for wilderness survival.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
