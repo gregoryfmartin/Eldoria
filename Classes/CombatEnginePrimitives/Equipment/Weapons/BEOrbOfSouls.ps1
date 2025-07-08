113bf8040575887e7994c2ff1faa50cd878f309d
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:42:05 -0400

    Added BEOrbOfSouls

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEOrbOfSouls.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ORB OF SOULS
+#
+###############################################################################
+
+Class BEOrbOfSouls : BEWeapon {
+	BEOrbOfSouls() : base() {
+		$this.Name          = 'Orb of Souls'
+		$this.MapObjName    = 'orbofsouls'
+		$this.PurchasePrice = 3600
+		$this.SellPrice     = 1800
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 10
+			[StatId]::MagicAttack = 90
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'An orb that can summon spirits to aid in battle.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
