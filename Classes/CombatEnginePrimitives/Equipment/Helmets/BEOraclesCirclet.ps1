559ff2c8f7a0c6ee1a00a9d6a560a543d0bdab63
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 14:07:37 -0400

    Added BEOraclesCirclet

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEOraclesCirclet.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ORACLE'S CIRCLET
+#
+###############################################################################
+
+Class BEOraclesCirclet : BEHelmet {
+	BEOraclesCirclet() : base() {
+		$this.Name               = 'Oracle''s Circlet'
+		$this.MapObjName         = 'oraclescirclet'
+		$this.PurchasePrice      = 1600
+		$this.SellPrice          = 800
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A shimmering circlet worn by oracles, granting glimpses of the future.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
