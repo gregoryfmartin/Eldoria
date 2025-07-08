bd6dc350e661209d0167c688e83d6c2c216199a4
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 13:58:11 -0400

    Added BESentienlsHelmet

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BESentinelsHelmet.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SENTINEL'S HELMET
+#
+###############################################################################
+
+Class BESentinelsHelmet : BEHelmet {
+	BESentinelsHelmet() : base() {
+		$this.Name               = 'Sentinel''s Helmet'
+		$this.MapObjName         = 'sentinelshelmet'
+		$this.PurchasePrice      = 750
+		$this.SellPrice          = 375
+		$this.TargetStats        = @{
+			[StatId]::Defense = 11
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A heavy, defensive helmet worn by vigilant sentinels, guarding key locations.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
