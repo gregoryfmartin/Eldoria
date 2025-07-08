0fe5fe219c29d51f6884c8de1eb21c5cf0e55ae2
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:46:04 -0400

    Added BEDreamcatcherStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEDreamcatcherStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DREAMCATCHER STAFF
+#
+###############################################################################
+
+Class BEDreamcatcherStaff : BEWeapon {
+	BEDreamcatcherStaff() : base() {
+		$this.Name          = 'Dreamcatcher Staff'
+		$this.MapObjName    = 'dreamcatcherstaff'
+		$this.PurchasePrice = 3700
+		$this.SellPrice     = 1850
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 10
+			[StatId]::MagicAttack = 85
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff that can put enemies into a deep sleep or manipulate their dreams.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
