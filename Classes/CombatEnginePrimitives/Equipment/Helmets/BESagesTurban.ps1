dbb1346e581bae1abfdd134faf8b70e9e40bf6ea
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 14:02:54 -0400

    Added BESagesTurban

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BESagesTurban.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SAGE'S TURBAN
+#
+###############################################################################
+
+Class BESagesTurban : BEHelmet {
+	BESagesTurban() : base() {
+		$this.Name               = 'Sage''s Turban'
+		$this.MapObjName         = 'sagesturban'
+		$this.PurchasePrice      = 1100
+		$this.SellPrice          = 550
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A wise turban worn by sages, imbued with ancient knowledge.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
