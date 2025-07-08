a1b2c82addeddb9274146b693c3b85e7c14af768
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 13:58:38 -0400

    Added BEMinstrelsCap

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEMinstrelsCap.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE MINSTREL'S CAP
+#
+###############################################################################
+
+Class BEMinstrelsCap : BEHelmet {
+	BEMinstrelsCap() : base() {
+		$this.Name               = 'Minstrel''s Cap'
+		$this.MapObjName         = 'minstrelscap'
+		$this.PurchasePrice      = 40
+		$this.SellPrice          = 20
+		$this.TargetStats        = @{
+			[StatId]::Defense = 1
+			[StatId]::MagicDefense = 1
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A whimsical cap worn by minstrels, adding charm to their performances.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
