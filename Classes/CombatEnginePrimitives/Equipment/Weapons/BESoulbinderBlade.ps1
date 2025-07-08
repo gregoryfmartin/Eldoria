c37378818344b3d9e6f87ddba22c2dc7a5c61182
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:12:33 -0400

    Added BESoulbinderBlade

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BESoulbinderBlade.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SOULBINDER BLADE
+#
+###############################################################################
+
+Class BESoulbinderBlade : BEWeapon {
+	BESoulbinderBlade() : base() {
+		$this.Name          = 'Soulbinder Blade'
+		$this.MapObjName    = 'soulbinderblade'
+		$this.PurchasePrice = 5800
+		$this.SellPrice     = 2900
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 135
+			[StatId]::MagicAttack = 55
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A blade that can temporarily bind an enemy''s movements.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
