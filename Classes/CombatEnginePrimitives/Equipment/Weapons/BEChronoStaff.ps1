7f87ee2fb6e0d588b22602fcecc664966383b298
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:48:57 -0400

    Added BEChronoStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEChronoStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE CHRONO STAFF
+#
+###############################################################################
+
+Class BEChronoStaff : BEWeapon {
+	BEChronoStaff() : base() {
+		$this.Name          = 'Chrono Staff'
+		$this.MapObjName    = 'chronostaff'
+		$this.PurchasePrice = 4500
+		$this.SellPrice     = 2250
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 18
+			[StatId]::MagicAttack = 120
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff that can manipulate time, slowing enemies or speeding up allies.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
