20cd66f2c3d233055129054b9e32a1ece1d5f726
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 13:59:58 -0400

    Added BEAlchemistsGoggles

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEAlchemistsGoggles.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ALCHEMIST'S GOGGLES
+#
+###############################################################################
+
+Class BEAlchemistsGoggles : BEHelmet {
+	BEAlchemistsGoggles() : base() {
+		$this.Name               = 'Alchemist''s Goggles'
+		$this.MapObjName         = 'alchemistsgoggles'
+		$this.PurchasePrice      = 50
+		$this.SellPrice          = 25
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'Goggles designed to protect the eyes of alchemists during experiments.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
