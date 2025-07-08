f5a378009cc17dce5b2529e88eb800171c450998
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 09:59:14 -0400

    Added BESpiritKingsStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BESpiritKingsStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SPIRIT KINGS STAFF
+#
+###############################################################################
+
+Class BESpiritKingsStaff : BEWeapon {
+	BESpiritKingsStaff() : base() {
+		$this.Name          = 'Spirit King''s Staff'
+		$this.MapObjName    = 'spiritkingsstaff'
+		$this.PurchasePrice = 6200
+		$this.SellPrice     = 3100
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 45
+			[StatId]::MagicAttack = 175
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff once wielded by a king of spirits, commanding spectral allies.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
