e4cde88b34fa044678526bb6d8ddd748b9874ea3
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 22:01:31 -0400

    Added BEGolemSlayer

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEGolemSlayer.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GOLEM SLAYER
+#
+###############################################################################
+
+Class BEGolemSlayer : BEWeapon {
+	BEGolemSlayer() : base() {
+		$this.Name          = 'Golem Slayer'
+		$this.MapObjName    = 'golemslayer'
+		$this.PurchasePrice = 3600
+		$this.SellPrice     = 1800
+		$this.TargetStats   = @{
+			[StatId]::Attack = 90
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A specialized weapon designed to destroy constructs and golems.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
