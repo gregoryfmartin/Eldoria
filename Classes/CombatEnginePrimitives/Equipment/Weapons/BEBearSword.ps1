fb220acb1aedd268f811a69738629e7af322b39d
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:59:17 -0400

    Added BEBearSword

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEBearSword.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BEAR SWORD
+#
+###############################################################################
+
+Class BEBearSword : BEWeapon {
+    BEBearSword() : base() {
+        $this.Name               = 'Bear Sword'
+        $this.MapObjName         = 'bearsword'
+        $this.PurchasePrice      = 0
+        $this.SellPrice          = 0
+        $this.TargetStats        = @{
+            [StatId]::Attack      = 999
+            [StatId]::MagicAttack = 999
+        }
+        $this.CanAddToInventory  = $true
+        $this.ExamineString      = 'The ultimate in fuzzy offense.'
+        $this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+        $this.TargetGender       = [Gender]::Unisex
+    }
+}
