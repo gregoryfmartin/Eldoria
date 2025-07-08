f18b31a45eb519151556de9ae9909e237e2a8a97
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 14:09:16 -0400

    Added BEValkyriesHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEValkyriesHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE VALKYRIE'S HELM
+#
+###############################################################################
+
+Class BEValkyriesHelm : BEHelmet {
+	BEValkyriesHelm() : base() {
+		$this.Name               = 'Valkyrie''s Helm'
+		$this.MapObjName         = 'valkyrieshelm'
+		$this.PurchasePrice      = 1900
+		$this.SellPrice          = 950
+		$this.TargetStats        = @{
+			[StatId]::Defense = 17
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A winged helm worn by valkyries, granting them courage and strength.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
