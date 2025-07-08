9a0b18168fabea7a8d64e0be5040160561e76c9d
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:00:31 -0400

    Added BEThousandCutsDagger

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEThousandCutsDagger.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE THOUSAND CUTS DAGGER
+#
+###############################################################################
+
+Class BEThousandCutsDagger : BEWeapon {
+	BEThousandCutsDagger() : base() {
+		$this.Name          = 'Thousand Cuts Dagger'
+		$this.MapObjName    = 'thousandcutsdagger'
+		$this.PurchasePrice = 5300
+		$this.SellPrice     = 2650
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 120
+			[StatId]::MagicAttack = 45
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A dagger so sharp it feels like a thousand blades at once.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
