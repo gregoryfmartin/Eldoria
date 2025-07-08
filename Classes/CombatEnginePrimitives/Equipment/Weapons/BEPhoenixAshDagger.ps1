1a6c57e8950539b0d84d16fab076f24fe41ec219
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 22:06:52 -0400

    Added BEPhoenixAshDagger

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEPhoenixAshDagger.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE PHOENIX ASH DAGGER
+#
+###############################################################################
+
+Class BEPhoenixAshDagger : BEWeapon {
+	BEPhoenixAshDagger() : base() {
+		$this.Name          = 'Phoenix Ash Dagger'
+		$this.MapObjName    = 'phoenixashdagger'
+		$this.PurchasePrice = 5400
+		$this.SellPrice     = 2700
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 125
+			[StatId]::MagicAttack = 50
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A dagger made from the ashes of a phoenix, capable of burning foes.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
