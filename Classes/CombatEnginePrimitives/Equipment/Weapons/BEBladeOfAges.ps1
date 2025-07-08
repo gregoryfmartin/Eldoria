eac1653ba5bb9aa040db995e1fabeca64823d16d
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 22:04:21 -0400

    Added BEBladeOfAges

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEBladeOfAges.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BLADE OF AGES
+#
+###############################################################################
+
+Class BEBladeOfAges : BEWeapon {
+	BEBladeOfAges() : base() {
+		$this.Name          = 'Blade of Ages'
+		$this.MapObjName    = 'bladeofages'
+		$this.PurchasePrice = 5800
+		$this.SellPrice     = 2900
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 140
+			[StatId]::MagicAttack = 60
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sword said to have witnessed the dawn of time, granting wisdom.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
