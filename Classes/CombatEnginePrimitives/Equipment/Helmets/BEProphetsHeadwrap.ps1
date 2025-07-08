a3a7bebb2bbaeb70583f566736b4ffd84032aa68
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 14:08:17 -0400

    Added BEProphetsHeadwrap

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEProphetsHeadwrap.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE PROPHET'S HEADWRAP
+#
+###############################################################################
+
+Class BEProphetsHeadwrap : BEHelmet {
+	BEProphetsHeadwrap() : base() {
+		$this.Name               = 'Prophet''s Headwrap'
+		$this.MapObjName         = 'prophetsheadwrap'
+		$this.PurchasePrice      = 1200
+		$this.SellPrice          = 600
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 16
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A simple headwrap worn by prophets, aiding in divine communication.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
