f2ebcec49b0c985db18cac0597aab5091acc70fd
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:22:53 -0400

    Added BEFrostbiteAxe

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEFrostbiteAxe.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE FROSTBITE AXE
+#
+###############################################################################
+
+Class BEFrostbiteAxe : BEWeapon {
+	BEFrostbiteAxe() : base() {
+		$this.Name          = 'Frostbite Axe'
+		$this.MapObjName    = 'frostbiteaxe'
+		$this.PurchasePrice = 6400
+		$this.SellPrice     = 3200
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 153
+			[StatId]::MagicAttack = 38
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'An axe that inflicts severe frostbite, slowing enemies to a crawl.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
