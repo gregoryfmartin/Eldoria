8335010eebd606d1756972e54cfd2bcd2e6559f9
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 11:06:57 -0400

    Added BEArchersHood

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEArchersHood.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ARCHERS HOOD
+#
+###############################################################################
+
+Class BEArchersHood : BEHelmet {
+	BEArchersHood() : base() {
+		$this.Name               = 'Archer''s Hood'
+		$this.MapObjName         = 'archershood'
+		$this.PurchasePrice      = 100
+		$this.SellPrice          = 50
+		$this.TargetStats        = @{
+			[StatId]::Defense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A light hood that provides camouflage and enhances an archer''s precision.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
