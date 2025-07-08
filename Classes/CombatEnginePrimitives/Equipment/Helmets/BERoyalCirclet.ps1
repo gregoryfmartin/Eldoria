7b4509c5b50888cbbc319ce00400a3f9466e1989
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 11:04:55 -0400

    Added BERoyalCirclet

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BERoyalCirclet.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ROYAL CIRCLET
+#
+###############################################################################
+
+Class BERoyalCirclet : BEHelmet {
+	BERoyalCirclet() : base() {
+		$this.Name               = 'Royal Circlet'
+		$this.MapObjName         = 'royalcirclet'
+		$this.PurchasePrice      = 800
+		$this.SellPrice          = 400
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'An elegant circlet adorned with jewels, worn by royalty.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
