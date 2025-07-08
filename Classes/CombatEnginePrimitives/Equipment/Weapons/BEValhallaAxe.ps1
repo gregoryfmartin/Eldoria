78659c7e8a39f9e024d8c68c9e93a39ae445b8c3
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:37:15 -0400

    Added BEValhallaAxe

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEValhallaAxe.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE VALHALLA AXE
+#
+###############################################################################
+
+Class BEValhallaAxe : BEWeapon {
+	BEValhallaAxe() : base() {
+		$this.Name          = 'Valhalla Axe'
+		$this.MapObjName    = 'valhallaaxe'
+		$this.PurchasePrice = 4700
+		$this.SellPrice     = 2350
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 125
+			[StatId]::MagicAttack = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'An axe that sings in battle, inspiring allies and striking fear into foes.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
