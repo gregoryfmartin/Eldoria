496049972dbbf17f4537ac372deafed2ace75dad
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:59:12 -0400

    Added BESeraphicBlade

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BESeraphicBlade.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SERAPHIC BLADE
+#
+###############################################################################
+
+Class BESeraphicBlade : BEWeapon {
+	BESeraphicBlade() : base() {
+		$this.Name          = 'Seraphic Blade'
+		$this.MapObjName    = 'seraphicblade'
+		$this.PurchasePrice = 4900
+		$this.SellPrice     = 2450
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 118
+			[StatId]::MagicAttack = 45
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sword of celestial origin, imbued with divine light.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
