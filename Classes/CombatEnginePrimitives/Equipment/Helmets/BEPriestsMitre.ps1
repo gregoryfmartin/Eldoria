ab8f406b745743cf184b155bd7987c2acb292da9
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 13:55:22 -0400

    Added BEPriestsMitre

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEPriestsMitre.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE PRIESTS MITRE
+#
+###############################################################################
+
+Class BEPriestsMitre : BEHelmet {
+	BEPriestsMitre() : base() {
+		$this.Name               = 'Priest''s Mitre'
+		$this.MapObjName         = 'priestsmitre'
+		$this.PurchasePrice      = 180
+		$this.SellPrice          = 90
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A ceremonial mitre worn by priests, enhancing divine blessings.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
