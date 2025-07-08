f2dc1774678a4e90091e19e460675e1c4420f8a7
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 14:11:20 -0400

    Added BEDreadnoughtHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEDreadnoughtHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DREADNOUGHT HELM
+#
+###############################################################################
+
+Class BEDreadnoughtHelm : BEHelmet {
+	BEDreadnoughtHelm() : base() {
+		$this.Name               = 'Dreadnought Helm'
+		$this.MapObjName         = 'dreadnoughthelm'
+		$this.PurchasePrice      = 2500
+		$this.SellPrice          = 1250
+		$this.TargetStats        = @{
+			[StatId]::Defense = 20
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A massive, heavily armored helm designed for ultimate defense.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
