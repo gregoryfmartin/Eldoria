f0c1e62287c4456fdd1043ee1769ad4dc5227ee8
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:05:33 -0400

    Added BESoulreaverStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BESoulreaverStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SOULREAVER STAFF
+#
+###############################################################################
+
+Class BESoulreaverStaff : BEWeapon {
+	BESoulreaverStaff() : base() {
+		$this.Name          = 'Soulreaver Staff'
+		$this.MapObjName    = 'soulreaverstaff'
+		$this.PurchasePrice = 6400
+		$this.SellPrice     = 3200
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 50
+			[StatId]::MagicAttack = 190
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff that rips souls from bodies, draining life force.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
