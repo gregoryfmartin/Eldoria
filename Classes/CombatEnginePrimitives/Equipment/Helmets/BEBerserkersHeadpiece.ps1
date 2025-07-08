6e389a907fb1f7496cfe3b40206ecab842dcf550
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 11:39:27 -0400

    Added BEBerserkersHeadpiece

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEBerserkersHeadpiece.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BERSERKERS HEADPIECE
+#
+###############################################################################
+
+Class BEBerserkersHeadpiece : BEHelmet {
+	BEBerserkersHeadpiece() : base() {
+		$this.Name               = 'Berserker''s Headpiece'
+		$this.MapObjName         = 'berserkersheadpiece'
+		$this.PurchasePrice      = 400
+		$this.SellPrice          = 200
+		$this.TargetStats        = @{
+			[StatId]::Defense = 13
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A brutal headpiece that amplifies a warrior''s raw strength.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Male
+	}
+}
