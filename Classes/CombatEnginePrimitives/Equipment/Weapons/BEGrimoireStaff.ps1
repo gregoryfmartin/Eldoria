96aae9daf7006bf9a7fe8363d58b01cd6530964a
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 22:03:25 -0400

    Added BEGrimoireStaff

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEGrimoireStaff.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GRIMOIRE STAFF
+#
+###############################################################################
+
+Class BEGrimoireStaff : BEWeapon {
+	BEGrimoireStaff() : base() {
+		$this.Name          = 'Grimoire Staff'
+		$this.MapObjName    = 'grimoirestaff'
+		$this.PurchasePrice = 5000
+		$this.SellPrice     = 2500
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 25
+			[StatId]::MagicAttack = 140
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A staff with an attached grimoire, granting access to powerful spells.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
