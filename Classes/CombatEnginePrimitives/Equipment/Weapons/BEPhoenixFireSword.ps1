01f0e0987f541ddc5dc66aeb1adcce2d926f850d
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 10:06:00 -0400

    Added BEPhoenixFireSword

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEPhoenixFireSword.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE PHOENIX FIRE SWORD
+#
+###############################################################################
+
+Class BEPhoenixFireSword : BEWeapon {
+	BEPhoenixFireSword() : base() {
+		$this.Name          = 'Phoenix Fire Sword'
+		$this.MapObjName    = 'phoenixfiresword'
+		$this.PurchasePrice = 6600
+		$this.SellPrice     = 3300
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 155
+			[StatId]::MagicAttack = 78
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sword that perpetually burns with phoenix fire, igniting foes.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
