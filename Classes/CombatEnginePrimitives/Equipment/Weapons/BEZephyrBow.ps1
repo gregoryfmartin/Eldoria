08aae9323323aa3f2403857ab3ffc97009397c60
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:57:12 -0400

    Added BEZephyrBow

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEZephyrBow.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ZEPHYR BOW
+#
+###############################################################################
+
+Class BEZephyrBow : BEWeapon {
+	BEZephyrBow() : base() {
+		$this.Name          = 'Zephyr Bow'
+		$this.MapObjName    = 'zephyrbow'
+		$this.PurchasePrice = 4300
+		$this.SellPrice     = 2150
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 105
+			[StatId]::MagicAttack = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A bow that allows arrows to travel at incredible speeds, almost instantly.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
