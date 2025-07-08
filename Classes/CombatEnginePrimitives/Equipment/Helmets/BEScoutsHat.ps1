075f3140440e2db2b935d048ffb4d6db6dc79cbc
Gregory F Martin <greg@gregfmartin.org>
2025-07-08 12:48:53 -0400

    Added BEScoutsHat

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEScoutsHat.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SCOUTS HAT
+#
+###############################################################################
+
+Class BEScoutsHat : BEHelmet {
+	BEScoutsHat() : base() {
+		$this.Name               = 'Scout''s Hat'
+		$this.MapObjName         = 'scoutshat'
+		$this.PurchasePrice      = 60
+		$this.SellPrice          = 30
+		$this.TargetStats        = @{
+			[StatId]::Defense = 2
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A lightweight hat for scouts, providing good visibility and protection from elements.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
