6109fe5b609b8b9f344109e44c493fffac153a64
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 21:31:36 -0400

    Added BESpiritBlade

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BESpiritBlade.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SPIRIT BLADE
+#
+###############################################################################
+
+Class BESpiritBlade : BEWeapon {
+	BESpiritBlade() : base() {
+		$this.Name          = 'Spirit Blade'
+		$this.MapObjName    = 'spiritblade'
+		$this.PurchasePrice = 1080
+		$this.SellPrice     = 540
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 54
+			[StatId]::MagicAttack = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sword that can harm incorporeal beings.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
