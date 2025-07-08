3c091153296c04639e9a76224b5103846c8e9cda
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 22:07:23 -0400

    Added BEHammerOfJudgment

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEHammerOfJudgment.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HAMMER OF JUDGMENT
+#
+###############################################################################
+
+Class BEHammerOfJudgment : BEWeapon {
+	BEHammerOfJudgment() : base() {
+		$this.Name          = 'Hammer of Judgment'
+		$this.MapObjName    = 'hammerofjudgment'
+		$this.PurchasePrice = 6300
+		$this.SellPrice     = 3150
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 165
+			[StatId]::MagicAttack = 20
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A colossal hammer that delivers righteous judgment, capable of stunning.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
