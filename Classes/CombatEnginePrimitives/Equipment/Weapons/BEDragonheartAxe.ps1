0a525c2fc6e5df4a8d020ca309bf75af699bbfe2
Gregory F Martin <greg@gregfmartin.org>
2025-07-07 22:05:18 -0400

    Added BEDragonheartAxe

---
+++ Classes/CombatEnginePrimitives/Equipment/Weapons/BEDragonheartAxe.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DRAGONHEART AXE
+#
+###############################################################################
+
+Class BEDragonheartAxe : BEWeapon {
+	BEDragonheartAxe() : base() {
+		$this.Name          = 'Dragonheart Axe'
+		$this.MapObjName    = 'dragonheartaxe'
+		$this.PurchasePrice = 6200
+		$this.SellPrice     = 3100
+		$this.TargetStats   = @{
+			[StatId]::Attack      = 155
+			[StatId]::MagicAttack = 30
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'An axe embedded with a dragon''s heart, pulsating with power.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Attack]) ATK  +$($this.TargetStats[[StatId]::MagicAttack]) MAT"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
