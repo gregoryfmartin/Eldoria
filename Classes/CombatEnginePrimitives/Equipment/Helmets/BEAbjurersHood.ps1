cdc983ffb84bb7e72fd2bc20db1c6d244907671b
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:15:39 -0400

    Added BEAbjurersHood

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEAbjurersHood.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ABJURER'S HOOD
+#
+###############################################################################
+
+Class BEAbjurersHood : BEHelmet {
+	BEAbjurersHood() : base() {
+		$this.Name               = 'Abjurer''s Hood'
+		$this.MapObjName         = 'abjurershood'
+		$this.PurchasePrice      = 1300
+		$this.SellPrice          = 650
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A hood worn by abjurers, specializing in defensive magic.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
