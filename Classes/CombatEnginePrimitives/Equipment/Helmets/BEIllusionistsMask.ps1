f4f226cc4dd499a3589937611b5e0905f6a652f1
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:18:19 -0400

    Added BEIllusionistsMask

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEIllusionistsMask.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ILLUSIONIST'S MASK
+#
+###############################################################################
+
+Class BEIllusionistsMask : BEHelmet {
+	BEIllusionistsMask() : base() {
+		$this.Name               = 'Illusionist''s Mask'
+		$this.MapObjName         = 'illusionistsmask'
+		$this.PurchasePrice      = 1700
+		$this.SellPrice          = 850
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 19
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A mask worn by illusionists, making their illusions more convincing.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
