70dcab7844aad928f161d7c2c997777e4ccfa1a5
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 10:09:35 -0400

    Added BEDreamthornHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEDreamthronHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DREAMTHORN HELM
+#
+###############################################################################
+
+Class BEDreamthornHelm : BEHelmet {
+	BEDreamthornHelm() : base() {
+		$this.Name               = 'Dreamthorn Helm'
+		$this.MapObjName         = 'dreamthornhelm'
+		$this.PurchasePrice      = 1800
+		$this.SellPrice          = 900
+		$this.TargetStats        = @{
+			[StatId]::Defense = 14
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm with sharp, dream-infused thorns, inflicting nightmares on enemies.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
