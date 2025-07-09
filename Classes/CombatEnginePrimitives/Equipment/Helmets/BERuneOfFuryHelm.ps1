75a814d703f2e6ba733aef56ade5f519b91ce1f2
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:20:19 -0400

    Added BERuneOfFuryHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BERuneOfFuryHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE RUNE OF FURY HELM
+#
+###############################################################################
+
+Class BERuneOfFuryHelm : BEHelmet {
+	BERuneOfFuryHelm() : base() {
+		$this.Name               = 'Rune of Fury Helm'
+		$this.MapObjName         = 'runeoffuryhelm'
+		$this.PurchasePrice      = 2000
+		$this.SellPrice          = 1000
+		$this.TargetStats        = @{
+			[StatId]::Defense = 22
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm inscribed with a rune of fury, increasing attack at low health.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Male
+	}
+}
