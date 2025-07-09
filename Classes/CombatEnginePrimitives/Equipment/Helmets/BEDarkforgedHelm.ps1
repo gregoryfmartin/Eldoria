17206bd535b6245911b10f3d92fad576ce5479f1
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:16:23 -0400

    Added BEDarkforgedHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEDarkforgedHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DARKFORGED HELM
+#
+###############################################################################
+
+Class BEDarkforgedHelm : BEHelmet {
+	BEDarkforgedHelm() : base() {
+		$this.Name               = 'Darkforged Helm'
+		$this.MapObjName         = 'darkforgedhelm'
+		$this.PurchasePrice      = 2100
+		$this.SellPrice          = 1050
+		$this.TargetStats        = @{
+			[StatId]::Defense = 15
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm forged in darkness, powerful against light creatures.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
