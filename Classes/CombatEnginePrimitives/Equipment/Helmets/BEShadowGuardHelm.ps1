9d566e7fa52fe25d274fa8976044596b504136cf
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:06:45 -0400

    Added BEShadowGuardHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEShadowGuardHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SHADOW GUARD HELM
+#
+###############################################################################
+
+Class BEShadowGuardHelm : BEHelmet {
+	BEShadowGuardHelm() : base() {
+		$this.Name               = 'Shadow Guard Helm'
+		$this.MapObjName         = 'shadowguardhelm'
+		$this.PurchasePrice      = 1350
+		$this.SellPrice          = 675
+		$this.TargetStats        = @{
+			[StatId]::Defense = 10
+			[StatId]::MagicDefense = 6
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A dark helm worn by shadow guards, aiding in stealth and ambush.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
