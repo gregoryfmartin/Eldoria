91ab21ad707f343087dd651fb46fd4e4c19d9a7c
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:22:31 -0400

    Added BEDivineHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEDivineHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DIVINE HELM
+#
+###############################################################################
+
+Class BEDivineHelm : BEHelmet {
+	BEDivineHelm() : base() {
+		$this.Name               = 'Divine Helm'
+		$this.MapObjName         = 'divinehelm'
+		$this.PurchasePrice      = 4500
+		$this.SellPrice          = 2250
+		$this.TargetStats        = @{
+			[StatId]::Defense = 40
+			[StatId]::MagicDefense = 30
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm of divine origin, granting godly powers.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
