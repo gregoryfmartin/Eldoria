553ce423110158b6fcaa76bfce02fb3393e6f031
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 10:09:56 -0400

    Added BEWispwoodHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEWispwoodHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE WISPWOOD HELM
+#
+###############################################################################
+
+Class BEWispwoodHelm : BEHelmet {
+	BEWispwoodHelm() : base() {
+		$this.Name               = 'Wispwood Helm'
+		$this.MapObjName         = 'wispwoodhelm'
+		$this.PurchasePrice      = 900
+		$this.SellPrice          = 450
+		$this.TargetStats        = @{
+			[StatId]::Defense = 5
+			[StatId]::MagicDefense = 7
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A light helm made from wispwood, granting improved awareness.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
