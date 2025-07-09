f8a3797748b642888b7e79a3035404cca9c015fa
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:33:20 -0400

    Added BEBasiliskScaleHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEBasiliskScaleHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BASILISK SCALE HELM
+#
+###############################################################################
+
+Class BEBasiliskScaleHelm : BEHelmet {
+	BEBasiliskScaleHelm() : base() {
+		$this.Name               = 'Basilisk Scale Helm'
+		$this.MapObjName         = 'basiliskscalehelm'
+		$this.PurchasePrice      = 1500
+		$this.SellPrice          = 750
+		$this.TargetStats        = @{
+			[StatId]::Defense = 12
+			[StatId]::MagicDefense = 8
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm made from basilisk scales, offering resistance to petrification.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
