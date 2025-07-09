27aab196cef18246532ceab90162384d7f291f0a
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:10:38 -0400

    Added BECursedHeadband

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BECursedHeadband.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE CURSED HEADBAND
+#
+###############################################################################
+
+Class BECursedHeadband : BEHelmet {
+	BECursedHeadband() : base() {
+		$this.Name               = 'Cursed Headband'
+		$this.MapObjName         = 'cursedheadband'
+		$this.PurchasePrice      = 70
+		$this.SellPrice          = 35
+		$this.TargetStats        = @{
+			[StatId]::Defense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A headband imbued with a minor curse, granting some power at a cost.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
