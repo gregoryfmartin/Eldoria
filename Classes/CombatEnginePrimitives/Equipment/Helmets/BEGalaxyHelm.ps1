b8179cb05163266dbcc45857228f6c76c45cc262
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:05:07 -0400

    Added BEGalaxyHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEGalaxyHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GALAXY HELM
+#
+###############################################################################
+
+Class BEGalaxyHelm : BEHelmet {
+	BEGalaxyHelm() : base() {
+		$this.Name               = 'Galaxy Helm'
+		$this.MapObjName         = 'galaxyhelm'
+		$this.PurchasePrice      = 8000
+		$this.SellPrice          = 4000
+		$this.TargetStats        = @{
+			[StatId]::Defense = 50
+			[StatId]::MagicDefense = 50
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm that contains a swirling galaxy within, granting immense power.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
