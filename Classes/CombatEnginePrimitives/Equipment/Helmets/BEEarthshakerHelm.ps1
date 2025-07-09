3bf5ff450f75fb4dc1aa25842d2f7067adf195e5
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:01:45 -0400

    Added BEEarthshakerHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEEarthshakerHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE EARTHSHAKER HELM
+#
+###############################################################################
+
+Class BEEarthshakerHelm : BEHelmet {
+	BEEarthshakerHelm() : base() {
+		$this.Name               = 'Earthshaker Helm'
+		$this.MapObjName         = 'earthshakerhelm'
+		$this.PurchasePrice      = 1000
+		$this.SellPrice          = 500
+		$this.TargetStats        = @{
+			[StatId]::Defense = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A heavy helm that resonates with the power of the earth, increasing physical might.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
