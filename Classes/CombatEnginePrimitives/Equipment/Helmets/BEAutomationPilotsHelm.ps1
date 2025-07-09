78d59301f43d7c284a23ba76e80a1dbb78c117c3
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:38:57 -0400

    Added BEAutomationPilotsHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEAutomationPilotsHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE AUTOMATION PILOTS HELM
+#
+###############################################################################
+
+Class BEAutomatonPilotsHelm : BEHelmet {
+	BEAutomatonPilotsHelm() : base() {
+		$this.Name               = 'Automaton Pilot''s Helm'
+		$this.MapObjName         = 'automatonpilotshelm'
+		$this.PurchasePrice      = 1900
+		$this.SellPrice          = 950
+		$this.TargetStats        = @{
+			[StatId]::Defense = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm for piloting large automatons, providing necessary controls.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
