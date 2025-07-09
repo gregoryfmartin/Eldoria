db68dd0197b069cbc383d748d1e71c6c1ced462a
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:03:59 -0400

    Added BEStardustCirclet

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEStardustCirclet.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE STARDUST CIRCLET
+#
+###############################################################################
+
+Class BEStardustCirclet : BEHelmet {
+	BEStardustCirclet() : base() {
+		$this.Name               = 'Stardust Circlet'
+		$this.MapObjName         = 'stardustcirclet'
+		$this.PurchasePrice      = 3500
+		$this.SellPrice          = 1750
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 28
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A circlet crafted from condensed stardust, granting cosmic awareness.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
