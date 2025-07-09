6531e04bd97656f4a501916ce2f81a2be5c36db3
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:18:03 -0400

    Added BERuneOfProtectionHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BERuneOfProtectionHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE RUNE OF PROTECTION HELM
+#
+###############################################################################
+
+Class BERuneOfProtectionHelm : BEHelmet {
+	BERuneOfProtectionHelm() : base() {
+		$this.Name               = 'Rune of Protection Helm'
+		$this.MapObjName         = 'runeofprotectionhelm'
+		$this.PurchasePrice      = 1900
+		$this.SellPrice          = 950
+		$this.TargetStats        = @{
+			[StatId]::Defense = 10
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm inscribed with a rune of protection, significantly increasing defense.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
