0c4af4635b3732fd78a8998464f4e2bb531e79d7
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:25:35 -0400

    Added BESirensHeadband

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BESirensHeadband.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SIRENS HEADBAND
+#
+###############################################################################
+
+Class BESirensHeadband : BEHelmet {
+	BESirensHeadband() : base() {
+		$this.Name               = 'Siren''s Headband'
+		$this.MapObjName         = 'sirensheadband'
+		$this.PurchasePrice      = 150
+		$this.SellPrice          = 75
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 7
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A shimmering headband worn by sirens, subtly enhancing their enchanting voices.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
