cbd901976f6885d5d11106bf4370187472e10e34
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 08:54:35 -0400

    Added BESunstoneCirclet

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BESunstoneCirclet.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SUNSTONE CIRCLET
+#
+###############################################################################
+
+Class BESunstoneCirclet : BEHelmet {
+	BESunstoneCirclet() : base() {
+		$this.Name               = 'Sunstone Circlet'
+		$this.MapObjName         = 'sunstonecirclet'
+		$this.PurchasePrice      = 800
+		$this.SellPrice          = 400
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 12
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A radiant circlet made of sunstone, imbued with healing light.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
