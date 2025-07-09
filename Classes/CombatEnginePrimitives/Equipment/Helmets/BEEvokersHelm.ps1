a8c349f93170c27792869bfdc0ccf3c3edb3d778
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:18:42 -0400

    Added BEEvokersHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEEvokersHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE EVOKER'S HELM
+#
+###############################################################################
+
+Class BEEvokersHelm : BEHelmet {
+	BEEvokersHelm() : base() {
+		$this.Name               = 'Evoker''s Helm'
+		$this.MapObjName         = 'evokershelm'
+		$this.PurchasePrice      = 1800
+		$this.SellPrice          = 900
+		$this.TargetStats        = @{
+			[StatId]::Defense = 5
+			[StatId]::MagicDefense = 20
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm worn by evokers, amplifying their destructive spells.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
