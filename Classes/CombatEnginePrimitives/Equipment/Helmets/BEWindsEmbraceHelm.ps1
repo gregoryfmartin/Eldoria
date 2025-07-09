bddfd919fd809e1544fe88120ea45a0a712d5765
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:15:13 -0400

    Added BEWindsEmbraceHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEWindsEmbraceHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE WIND'S EMBRACE HELM
+#
+###############################################################################
+
+Class BEWindsEmbraceHelm : BEHelmet {
+	BEWindsEmbraceHelm() : base() {
+		$this.Name               = 'Wind''s Embrace Helm'
+		$this.MapObjName         = 'windsembracehelm'
+		$this.PurchasePrice      = 1600
+		$this.SellPrice          = 800
+		$this.TargetStats        = @{
+			[StatId]::Defense = 10
+			[StatId]::MagicDefense = 8
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A light helm that allows the wearer to move with the swiftness of wind.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
