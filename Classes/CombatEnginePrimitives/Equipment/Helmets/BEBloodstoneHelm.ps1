3f47ad42b2a571ee4fcad5e4fdff45b55b411ceb
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 10:57:22 -0400

    Added BEBloodstoneHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEBloodstoneHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BLOODSTONE HELM
+#
+###############################################################################
+
+Class BEBloodstoneHelm : BEHelmet {
+	BEBloodstoneHelm() : base() {
+		$this.Name               = 'Bloodstone Helm'
+		$this.MapObjName         = 'bloodstonehelm'
+		$this.PurchasePrice      = 1600
+		$this.SellPrice          = 800
+		$this.TargetStats        = @{
+			[StatId]::Defense = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A grim helm with bloodstone, enhancing offensive capabilities at a cost to health.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
