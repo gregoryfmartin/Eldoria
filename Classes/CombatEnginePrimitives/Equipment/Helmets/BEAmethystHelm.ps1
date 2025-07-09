4214661c4dc8859f5d5f55fdf92d872e88efb393
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 10:23:16 -0400

    Added BEAmethystHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEAmethystHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE AMETHYST HELM
+#
+###############################################################################
+
+Class BEAmethystHelm : BEHelmet {
+	BEAmethystHelm() : base() {
+		$this.Name               = 'Amethyst Helm'
+		$this.MapObjName         = 'amethysthelm'
+		$this.PurchasePrice      = 1600
+		$this.SellPrice          = 800
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 16
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm with an amethyst, enhancing mental clarity and resistance to mind-control.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
