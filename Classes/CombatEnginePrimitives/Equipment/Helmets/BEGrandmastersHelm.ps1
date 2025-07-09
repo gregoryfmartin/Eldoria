f8c7bb4909eb4bfb127336eedd08259dfa781628
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:08:43 -0400

    Added BEGrandmastersHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEGrandmastersHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GRANDMASTER'S HELM
+#
+###############################################################################
+
+Class BEGrandmastersHelm : BEHelmet {
+	BEGrandmastersHelm() : base() {
+		$this.Name               = 'Grandmaster''s Helm'
+		$this.MapObjName         = 'grandmastershelm'
+		$this.PurchasePrice      = 2800
+		$this.SellPrice          = 1400
+		$this.TargetStats        = @{
+			[StatId]::Defense = 25
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A legendary helm worn by grandmasters of a martial art or order.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
