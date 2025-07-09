24dbb6b5b5f2da148a73431f5a322fe5dcb59c92
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:08:24 -0400

    Added BEVoidwalkerHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEVoidwalkerHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE VOIDWALKER HELM
+#
+###############################################################################
+
+Class BEVoidwalkerHelm : BEHelmet {
+	BEVoidwalkerHelm() : base() {
+		$this.Name               = 'Voidwalker Helm'
+		$this.MapObjName         = 'voidwalkerhelm'
+		$this.PurchasePrice      = 5500
+		$this.SellPrice          = 2750
+		$this.TargetStats        = @{
+			[StatId]::Defense = 25
+			[StatId]::MagicDefense = 30
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm for those who walk the void, granting protection from its horrors.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
