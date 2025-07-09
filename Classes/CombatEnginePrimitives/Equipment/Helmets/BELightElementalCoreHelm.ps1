ef2f78fd247ca4e44e12080354f851cc31853b4c
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:56:15 -0400

    Added BELightElementalCoreHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BELightElementalCoreHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE LIGHT ELEMENTAL CORE HELM
+#
+###############################################################################
+
+Class BELightElementalCoreHelm : BEHelmet {
+	BELightElementalCoreHelm() : base() {
+		$this.Name               = 'Light Elemental Core Helm'
+		$this.MapObjName         = 'lightelementalcorehelm'
+		$this.PurchasePrice      = 2500
+		$this.SellPrice          = 1250
+		$this.TargetStats        = @{
+			[StatId]::Defense = 5
+			[StatId]::MagicDefense = 20
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm infused with a light elemental core, radiating holy energy.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
