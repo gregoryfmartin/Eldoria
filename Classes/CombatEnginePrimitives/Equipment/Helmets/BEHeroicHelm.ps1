d7d3e52af0a1d24881840b900000cc6bf36576f2
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:21:17 -0400

    Added BEHeroicHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEHeroicHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HEROIC HELM
+#
+###############################################################################
+
+Class BEHeroicHelm : BEHelmet {
+	BEHeroicHelm() : base() {
+		$this.Name               = 'Heroic Helm'
+		$this.MapObjName         = 'heroichelm'
+		$this.PurchasePrice      = 3000
+		$this.SellPrice          = 1500
+		$this.TargetStats        = @{
+			[StatId]::Defense = 25
+			[StatId]::MagicDefense = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm of ancient heroes, inspiring allies and striking fear in foes.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Male
+	}
+}
