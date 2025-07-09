7df5c49a03958f6040b7a8cbc5ab61891e4d5b6e
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:41:00 -0400

    Added BELichsPhylacteryHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BELichsPhylacteryHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE LICH'S PHYLACTERY HELM
+#
+###############################################################################
+
+Class BELichsPhylacteryHelm : BEHelmet {
+	BELichsPhylacteryHelm() : base() {
+		$this.Name               = 'Lich''s Phylactery Helm'
+		$this.MapObjName         = 'lichsphylacteryhelm'
+		$this.PurchasePrice      = 2500
+		$this.SellPrice          = 1250
+		$this.TargetStats        = @{
+			[StatId]::Defense = 5
+			[StatId]::MagicDefense = 25
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A cursed helm containing a lich''s phylactery, granting dark powers.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
