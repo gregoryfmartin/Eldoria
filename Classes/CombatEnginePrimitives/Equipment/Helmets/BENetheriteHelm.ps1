8aed016b77ad26b97e84d87c686f23373129c329
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:02:54 -0400

    Added BENetheriteHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BENetheriteHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE NETHERITE HELM
+#
+###############################################################################
+
+Class BENetheriteHelm : BEHelmet {
+	BENetheriteHelm() : base() {
+		$this.Name               = 'Netherite Helm'
+		$this.MapObjName         = 'netheritehelm'
+		$this.PurchasePrice      = 4800
+		$this.SellPrice          = 2400
+		$this.TargetStats        = @{
+			[StatId]::Defense = 32
+			[StatId]::MagicDefense = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm forged from netherite, resistant to fire and powerful in the nether.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
