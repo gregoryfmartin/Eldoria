83a435a959dd53006b62f3b70f18595ff2c2bcc4
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:20:57 -0400

    Added BERuneOfSerenityHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BERuneOfSerenityHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE RUNE OF SERENITY HELM
+#
+###############################################################################
+
+Class BERuneOfSerenityHelm : BEHelmet {
+	BERuneOfSerenityHelm() : base() {
+		$this.Name               = 'Rune of Serenity Helm'
+		$this.MapObjName         = 'runeofserenityhelm'
+		$this.PurchasePrice      = 2000
+		$this.SellPrice          = 1000
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 22
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm inscribed with a rune of serenity, calming the mind and boosting magic.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
