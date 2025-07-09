a1e707cdda9c2c0db3f2e51de3d2be2122d2cb9c
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 10:19:18 -0400

    Added BEJadeCrown

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEJadeCrown.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE JADE CROWN
+#
+###############################################################################
+
+Class BEJadeCrown : BEHelmet {
+	BEJadeCrown() : base() {
+		$this.Name               = 'Jade Crown'
+		$this.MapObjName         = 'jadecrown'
+		$this.PurchasePrice      = 1900
+		$this.SellPrice          = 950
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A crown made of pure jade, enhancing wisdom and longevity.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
