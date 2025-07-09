ce93390d0df097ba73e8617107a260268c3b8f18
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:05:02 -0400

    Added BEGolemsHeadpiece

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEGolemsHeadpiece.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GOLEMS HEADPIECE
+#
+###############################################################################
+
+Class BEGolemsHeadpiece : BEHelmet {
+	BEGolemsHeadpiece() : base() {
+		$this.Name               = 'Golem''s Headpiece'
+		$this.MapObjName         = 'golemsheadpiece'
+		$this.PurchasePrice      = 1800
+		$this.SellPrice          = 900
+		$this.TargetStats        = @{
+			[StatId]::Defense = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sturdy headpiece made from golem fragments, offering immense durability.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
