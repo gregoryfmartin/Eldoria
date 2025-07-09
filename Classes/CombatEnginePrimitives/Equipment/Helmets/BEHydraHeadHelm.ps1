2c8c0e1f6f01319b9e62c5b4053a9bfa723815fe
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:38:27 -0400

    Added BEHydraHeadHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEHydraHeadHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HYDRA HEAD HELM
+#
+###############################################################################
+
+Class BEHydraHeadHelm : BEHelmet {
+	BEHydraHeadHelm() : base() {
+		$this.Name               = 'Hydra Head Helm'
+		$this.MapObjName         = 'hydraheadhelm'
+		$this.PurchasePrice      = 1900
+		$this.SellPrice          = 950
+		$this.TargetStats        = @{
+			[StatId]::Defense = 15
+			[StatId]::MagicDefense = 9
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A monstrous helm adorned with a hydra head, granting multiple perspectives.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
