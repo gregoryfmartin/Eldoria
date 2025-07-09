34efce24f5092c941de7c64134fd80700203c5cc
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:42:06 -0400

    Added BEDemonsHornedHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEDEmonsHornedHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DEMON'S HORNED HELM
+#
+###############################################################################
+
+Class BEDemonsHornedHelm : BEHelmet {
+	BEDemonsHornedHelm() : base() {
+		$this.Name               = 'Demon''s Horned Helm'
+		$this.MapObjName         = 'demonshornedhelm'
+		$this.PurchasePrice      = 2800
+		$this.SellPrice          = 1400
+		$this.TargetStats        = @{
+			[StatId]::Defense = 22
+			[StatId]::MagicDefense = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A dark, horned helm imbued with demonic power, granting destructive capabilities.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Male
+	}
+}
