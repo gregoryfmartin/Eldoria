9cefa610d56a898783538fcc294712054d6c1c91
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:49:14 -0400

    Added BEZombiesBrainBucket

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEZombiesBrainBucket.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE ZOMBIE'S BRAIN BUCKET
+#
+###############################################################################
+
+Class BEZombiesBrainBucket : BEHelmet {
+	BEZombiesBrainBucket() : base() {
+		$this.Name               = 'Zombie''s Brain Bucket'
+		$this.MapObjName         = 'zombiesbrainbucket'
+		$this.PurchasePrice      = 100
+		$this.SellPrice          = 50
+		$this.TargetStats        = @{
+			[StatId]::Defense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A gruesome bucket worn by zombies, protecting their brains.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
