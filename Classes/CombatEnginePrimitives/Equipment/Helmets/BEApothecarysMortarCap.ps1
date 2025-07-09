4758ce64aaa7bdb80d0ad2aeb2602035e4e8bbf4
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:51:59 -0400

    Added BEApothecarysMortarCap

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEApothecarysMortarCap.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE APOTHECARY'S MORTAR CAP
+#
+###############################################################################
+
+Class BEApothecarysMortarCap : BEHelmet {
+	BEApothecarysMortarCap() : base() {
+		$this.Name               = 'Apothecary''s Mortar Cap'
+		$this.MapObjName         = 'apothecarysmortarcap'
+		$this.PurchasePrice      = 130
+		$this.SellPrice          = 65
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 7
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A cap worn by apothecaries, aiding in the creation of potent concoctions.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
