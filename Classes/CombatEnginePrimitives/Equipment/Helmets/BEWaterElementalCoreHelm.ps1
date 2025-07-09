8492743a0c3d9468c1bfd74ad9bb39126ec757d6
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:54:52 -0400

    Added BEWaterElementalCoreHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEWaterElementalCoreHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE WATER ELEMENTAL CORE HELM
+#
+###############################################################################
+
+Class BEWaterElementalCoreHelm : BEHelmet {
+	BEWaterElementalCoreHelm() : base() {
+		$this.Name               = 'Water Elemental Core Helm'
+		$this.MapObjName         = 'waterelementalcorehelm'
+		$this.PurchasePrice      = 2000
+		$this.SellPrice          = 1000
+		$this.TargetStats        = @{
+			[StatId]::Defense = 10
+			[StatId]::MagicDefense = 15
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm infused with a water elemental core, granting water breathing.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
