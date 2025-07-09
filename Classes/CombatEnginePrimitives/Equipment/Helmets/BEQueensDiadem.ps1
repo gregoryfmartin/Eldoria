97c7af2433e11ae59938554a91c9ac9890bcb670
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 14:16:01 -0400

    Added BEQueensDiadem

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEQueensDiadem.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE QUEEN'S DIADEM
+#
+###############################################################################
+
+Class BEQueensDiadem : BEHelmet {
+	BEQueensDiadem() : base() {
+		$this.Name               = 'Queen''s Diadem'
+		$this.MapObjName         = 'queensdiadem'
+		$this.PurchasePrice      = 2500
+		$this.SellPrice          = 1250
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A beautiful diadem worn by queens, symbolizing wisdom and benevolence.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
