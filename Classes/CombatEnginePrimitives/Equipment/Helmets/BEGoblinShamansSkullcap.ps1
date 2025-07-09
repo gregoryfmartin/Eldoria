a015a272947a6f10b60bdd1945c83cc50f51cf03
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:44:40 -0400

    Added BEGoblinShamansSkullcap

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEGoblinShamansSkullcap.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GOBLIN SHAMAN'S SKULLCAP
+#
+###############################################################################
+
+Class BEGoblinShamansSkullcap : BEHelmet {
+	BEGoblinShamansSkullcap() : base() {
+		$this.Name               = 'Goblin Shaman''s Skullcap'
+		$this.MapObjName         = 'goblinshamansskullcap'
+		$this.PurchasePrice      = 90
+		$this.SellPrice          = 45
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 5
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A crude skullcap adorned with goblin shaman trinkets, enhancing their rudimentary magic.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
