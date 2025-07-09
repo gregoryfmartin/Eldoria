86d5ead24095611ebb83134d49070e4a5f95272a
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 14:22:03 -0400

    Added BESergeantsHelmet

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BESergeantsHelmet.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SERGEANT'S HELMET
+#
+###############################################################################
+
+Class BESergeantsHelmet : BEHelmet {
+	BESergeantsHelmet() : base() {
+		$this.Name               = 'Sergeant''s Helmet'
+		$this.MapObjName         = 'sergeantshelmet'
+		$this.PurchasePrice      = 250
+		$this.SellPrice          = 125
+		$this.TargetStats        = @{
+			[StatId]::Defense = 8
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A practical helmet worn by sergeants, designed for frontline leadership.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
