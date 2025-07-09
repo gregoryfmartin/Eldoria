ab7453d69595588729f37b255b967062a586235e
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 10:24:04 -0400

    Added BEGarnetHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEGarnetHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GARNET HELM
+#
+###############################################################################
+
+Class BEGarnetHelm : BEHelmet {
+	BEGarnetHelm() : base() {
+		$this.Name               = 'Garnet Helm'
+		$this.MapObjName         = 'garnethelm'
+		$this.PurchasePrice      = 1500
+		$this.SellPrice          = 750
+		$this.TargetStats        = @{
+			[StatId]::Defense = 12
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm with a deep red garnet, boosting vitality and inner strength.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
