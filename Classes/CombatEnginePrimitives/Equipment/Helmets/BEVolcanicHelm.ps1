b57ad1b26aafe081c4593ec048b71e88926684af
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 08:57:05 -0400

    Added BEVolcanicHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEVolcanicHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE VOLCANIC HELM
+#
+###############################################################################
+
+Class BEVolcanicHelm : BEHelmet {
+	BEVolcanicHelm() : base() {
+		$this.Name               = 'Volcanic Helm'
+		$this.MapObjName         = 'volcanichelm'
+		$this.PurchasePrice      = 900
+		$this.SellPrice          = 450
+		$this.TargetStats        = @{
+			[StatId]::Defense = 10
+			[StatId]::MagicDefense = 7
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm forged in volcanic fire, granting resistance to heat.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
