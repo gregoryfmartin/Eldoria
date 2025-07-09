c741f804d8ef23061684e72846fee75872d55f4a
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:53:25 -0400

    Added BEChefsToqueHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEChefsToqueHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE CHEF'S TOQUE HELM
+#
+###############################################################################
+
+Class BEChefsToqueHelm : BEHelmet {
+	BEChefsToqueHelm() : base() {
+		$this.Name               = 'Chef''s Toque Helm'
+		$this.MapObjName         = 'chefstoquehelm'
+		$this.PurchasePrice      = 150
+		$this.SellPrice          = 75
+		$this.TargetStats        = @{
+			[StatId]::Defense = 4
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A surprisingly sturdy helm designed for battle-chefs, offering a surprising amount of protection.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
