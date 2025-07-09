5741756e1fe0f490473deaad35096769ff9805f2
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:02:07 -0400

    Added BESkywalkerHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BESkywalkerHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SKYWALKER HELM
+#
+###############################################################################
+
+Class BESkywalkerHelm : BEHelmet {
+	BESkywalkerHelm() : base() {
+		$this.Name               = 'Skywalker Helm'
+		$this.MapObjName         = 'skywalkerhelm'
+		$this.PurchasePrice      = 950
+		$this.SellPrice          = 475
+		$this.TargetStats        = @{
+			[StatId]::Defense = 8
+			[StatId]::MagicDefense = 6
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A lightweight helm that seems to defy gravity, aiding in agility.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
