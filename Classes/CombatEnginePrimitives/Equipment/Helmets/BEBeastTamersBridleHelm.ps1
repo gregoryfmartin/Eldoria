4b42e8a81cc073e104d05dfc13cb2232a8beb24d
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:33:00 -0400

    Added BEBeastTamersBridleHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEBeastTamersBridleHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BEAST TAMER'S BRIDLE HELM
+#
+###############################################################################
+
+Class BEBeastTamersBridleHelm : BEHelmet {
+	BEBeastTamersBridleHelm() : base() {
+		$this.Name               = 'Beast Tamer''s Bridle Helm'
+		$this.MapObjName         = 'beasttamersbridlehelm'
+		$this.PurchasePrice      = 1500
+		$this.SellPrice          = 750
+		$this.TargetStats        = @{
+			[StatId]::Defense = 5
+			[StatId]::MagicDefense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A unique helm that aids in taming wild beasts.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
