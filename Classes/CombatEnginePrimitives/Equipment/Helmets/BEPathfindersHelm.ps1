78b75929aff0252a3b8f6b951bed5bdb11906827
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:25:07 -0400

    Added BEPathfindersHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEPathfindersHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE PATHFINDER'S HELM
+#
+###############################################################################
+
+Class BEPathfindersHelm : BEHelmet {
+	BEPathfindersHelm() : base() {
+		$this.Name               = 'Pathfinder''s Helm'
+		$this.MapObjName         = 'pathfindershelm'
+		$this.PurchasePrice      = 150
+		$this.SellPrice          = 75
+		$this.TargetStats        = @{
+			[StatId]::Defense = 4
+			[StatId]::MagicDefense = 2
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm that helps pathfinders navigate treacherous terrain.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
