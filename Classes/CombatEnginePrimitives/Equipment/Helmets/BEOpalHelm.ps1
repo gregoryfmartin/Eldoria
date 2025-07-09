aaa48bdbbcfe3ce1f85faa3b6970bebcc96e1b3a
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 10:24:44 -0400

    Added BEOpalHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEOpalHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE OPAL HELM
+#
+###############################################################################
+
+Class BEOpalHelm : BEHelmet {
+	BEOpalHelm() : base() {
+		$this.Name               = 'Opal Helm'
+		$this.MapObjName         = 'opalhelm'
+		$this.PurchasePrice      = 1400
+		$this.SellPrice          = 700
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 14
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A shimmering helm with an opal, granting minor illusionary abilities.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
