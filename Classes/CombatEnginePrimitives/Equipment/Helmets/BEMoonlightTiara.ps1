878754a77cb5cd90681953340cab962cdfef16c7
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 08:55:01 -0400

    Added BEMoonlightTiara

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEMoonlightTiara.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE MOONLIGHT TIARA
+#
+###############################################################################
+
+Class BEMoonlightTiara : BEHelmet {
+	BEMoonlightTiara() : base() {
+		$this.Name               = 'Moonlight Tiara'
+		$this.MapObjName         = 'moonlighttiara'
+		$this.PurchasePrice      = 800
+		$this.SellPrice          = 400
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 12
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A luminous tiara that glows with moonlight, enhancing nocturnal magic.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Female
+	}
+}
