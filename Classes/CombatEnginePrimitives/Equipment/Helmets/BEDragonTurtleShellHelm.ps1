99b593c66d75ff724e53b9c2bf2389fb80216693
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:54:00 -0400

    Added BEDragonTurtleShellHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEDragonTurtleShellHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DRAGON TURTLE SHELL HELM
+#
+###############################################################################
+
+Class BEDragonTurtleShellHelm : BEHelmet {
+	BEDragonTurtleShellHelm() : base() {
+		$this.Name               = 'Dragon Turtle Shell Helm'
+		$this.MapObjName         = 'dragonturtleshellhelm'
+		$this.PurchasePrice      = 2200
+		$this.SellPrice          = 1100
+		$this.TargetStats        = @{
+			[StatId]::Defense = 18
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A massive helm made from a dragon turtle''s shell, offering exceptional defense.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
