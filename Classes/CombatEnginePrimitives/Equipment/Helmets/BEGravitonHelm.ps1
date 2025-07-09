3b92c3de4186ea5944a3ec06a8465b1847f44439
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 11:10:13 -0400

    Added BEGravitonHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEGravitonHelm.ps1
@@ -0,0 +1,26 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GRAVITON HELM
+#
+###############################################################################
+
+Class BEGravitonHelm : BEHelmet {
+	BEGravitonHelm() : base() {
+		$this.Name               = 'Graviton Helm'
+		$this.MapObjName         = 'gravitonhelm'
+		$this.PurchasePrice      = 5200
+		$this.SellPrice          = 2600
+		$this.TargetStats        = @{
+			[StatId]::Defense = 28
+			[StatId]::MagicDefense = 28
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A helm that manipulates gravity, allowing the wearer to float or crush foes.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF  +$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
