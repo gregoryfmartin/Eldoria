4f604a0870aff5d1807a9565e69079c9f6f4d64e
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:29:38 -0400

    Added BECEntaursHeadguard

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BECentaursHeadguard.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE CENTAUR'S HEADGUARD
+#
+###############################################################################
+
+Class BECentaursHeadguard : BEHelmet {
+	BECentaursHeadguard() : base() {
+		$this.Name               = 'Centaur''s Headguard'
+		$this.MapObjName         = 'centaursheadguard'
+		$this.PurchasePrice      = 600
+		$this.SellPrice          = 300
+		$this.TargetStats        = @{
+			[StatId]::Defense = 10
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A protective headguard designed for centaur warriors.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
