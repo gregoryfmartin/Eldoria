f2352fe641cc17d4403637b4303d8a2125c224f8
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 15:24:40 -0400

    Added BEPioneersHat

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEPioneersHat.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE PIONEER'S HAT
+#
+###############################################################################
+
+Class BEPioneersHat : BEHelmet {
+	BEPioneersHat() : base() {
+		$this.Name               = 'Pioneer''s Hat'
+		$this.MapObjName         = 'pioneershat'
+		$this.PurchasePrice      = 120
+		$this.SellPrice          = 60
+		$this.TargetStats        = @{
+			[StatId]::Defense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A sturdy hat for pioneers, essential for exploration.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
