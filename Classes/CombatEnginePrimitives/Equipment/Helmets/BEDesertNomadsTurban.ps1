9cfb31e27d0e865521ecad4ea5188c230c07ea52
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 08:57:57 -0400

    Added BEDesertNomadsTurban

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEDesertNomadsTurban.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE DESERT NOMADS TURBAN
+#
+###############################################################################
+
+Class BEDesertNomadsTurban : BEHelmet {
+	BEDesertNomadsTurban() : base() {
+		$this.Name               = 'Desert Nomad''s Turban'
+		$this.MapObjName         = 'desertnomadsturban'
+		$this.PurchasePrice      = 130
+		$this.SellPrice          = 65
+		$this.TargetStats        = @{
+			[StatId]::Defense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A practical turban for desert dwellers, protecting against sand and sun.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
