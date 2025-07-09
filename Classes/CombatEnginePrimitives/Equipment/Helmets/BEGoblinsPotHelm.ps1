028c26c8badb29205fc42e7000e649ab897eded1
Gregory F Martin <greg@gregfmartin.org>
2025-07-09 09:14:58 -0400

    Added BEGoblinsPotHelm

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEGoblinsPotHelm.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE GOBLINS POT HELM
+#
+###############################################################################
+
+Class BEGoblinsPotHelm : BEHelmet {
+	BEGoblinsPotHelm() : base() {
+		$this.Name               = 'Goblin''s Pot Helm'
+		$this.MapObjName         = 'goblinspothelm'
+		$this.PurchasePrice      = 30
+		$this.SellPrice          = 15
+		$this.TargetStats        = @{
+			[StatId]::Defense = 1
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A makeshift helmet fashioned from a cooking pot, offering minimal protection.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
