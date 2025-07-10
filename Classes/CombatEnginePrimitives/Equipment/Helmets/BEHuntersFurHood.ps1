3e04592ecba9fc6b76da495217dd921311a3f9fe
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:06:05 -0400

    Added BEHuntersFurHood

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEHuntersFurHood.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE HUNTER'S FUR HOOD
+#
+###############################################################################
+
+Class BEHuntersFurHood : BEHelmet {
+	BEHuntersFurHood() : base() {
+		$this.Name               = 'Hunter''s Fur Hood'
+		$this.MapObjName         = 'huntersfurhood'
+		$this.PurchasePrice      = 150
+		$this.SellPrice          = 75
+		$this.TargetStats        = @{
+			[StatId]::Defense = 5
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A warm fur hood for hunters, providing warmth and stealth in cold climates.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
