31af8d6f5dfe1795c4b028b05a318b5b9f9bd2e8
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:06:34 -0400

    Added BEFishermansKnitCap

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEFishermansKnitCap.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE FISHERMAN'S KNIT CAP
+#
+###############################################################################
+
+Class BEFishermansKnitCap : BEHelmet {
+	BEFishermansKnitCap() : base() {
+		$this.Name               = 'Fisherman''s Knit Cap'
+		$this.MapObjName         = 'fishermansknitcap'
+		$this.PurchasePrice      = 60
+		$this.SellPrice          = 30
+		$this.TargetStats        = @{
+			[StatId]::Defense = 1
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A warm knit cap for fishermen, warding off the cold.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
