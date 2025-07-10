b507a6349686d4d2ff87f6e2046e800c40470159
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:00:43 -0400

    Added BEFishermansHat

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEFishermansHat.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE FISHERMAN'S HAT
+#
+###############################################################################
+
+Class BEFishermansHat : BEHelmet {
+	BEFishermansHat() : base() {
+		$this.Name               = 'Fisherman''s Hat'
+		$this.MapObjName         = 'fishermanshat'
+		$this.PurchasePrice      = 50
+		$this.SellPrice          = 25
+		$this.TargetStats        = @{
+			[StatId]::Defense = 1
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A wide-brimmed hat that protects fishermen from the elements.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
