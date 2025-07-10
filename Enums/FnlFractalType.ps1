a6a0e4ea465d64cef562041aee0652f907f657d9
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 12:20:29 -0400

    Fixed enums and added FNL impl

---
+++ Enums/FnlFractalType.ps1
@@ -0,0 +1,12 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+Enum FnlFractalType {
+    None
+    FBm
+    Ridged
+    PingPong
+    DomainWarpProgressive
+    DomainWarpIndependent
+}
