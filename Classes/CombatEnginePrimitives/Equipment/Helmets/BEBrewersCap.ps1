2cfae0c35ead94120420cf37e8cc397b8c1ca7f4
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:08:09 -0400

    Added BEBrewersCap

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEBrewersCap.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BREWER'S CAP
+#
+###############################################################################
+
+Class BEBrewersCap : BEHelmet {
+	BEBrewersCap() : base() {
+		$this.Name               = 'Brewer''s Cap'
+		$this.MapObjName         = 'brewerscap'
+		$this.PurchasePrice      = 70
+		$this.SellPrice          = 35
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 1
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A traditional cap worn by brewers, ensuring good spirits.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
