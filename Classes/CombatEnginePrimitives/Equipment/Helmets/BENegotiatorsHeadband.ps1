c07f16e591cde1780adadbe674aaff7da3d135cd
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:36:16 -0400

    Added BENegotiatorsHeadband

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BENegotiatorsHeadband.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE NEGOTIATOR'S HEADBAND
+#
+###############################################################################
+
+Class BENegotiatorsHeadband : BEHelmet {
+	BENegotiatorsHeadband() : base() {
+		$this.Name               = 'Negotiator''s Headband'
+		$this.MapObjName         = 'negotiatorsheadband'
+		$this.PurchasePrice      = 110
+		$this.SellPrice          = 55
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A headband that aids negotiators in finding common ground.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
