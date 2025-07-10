95a98a54da04a18b6f3daa9805fb424e761f4ff2
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:38:55 -0400

    Added BESentrysHelmet

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BESentrysHelmet.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE SENTRY'S HELMET
+#
+###############################################################################
+
+Class BESentrysHelmet : BEHelmet {
+	BESentrysHelmet() : base() {
+		$this.Name               = 'Sentry''s Helmet'
+		$this.MapObjName         = 'sentryshelmet'
+		$this.PurchasePrice      = 180
+		$this.SellPrice          = 90
+		$this.TargetStats        = @{
+			[StatId]::Defense = 5
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A standard helmet for sentries, providing reliable protection.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
