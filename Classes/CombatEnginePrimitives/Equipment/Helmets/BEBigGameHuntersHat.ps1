2c00d9b9734530461091a56632e0af0720ec0fc9
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:17:10 -0400

    Added BEBigGameHuntersHat

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEBigGameHuntersHat.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE BIG GAME HUNTER'S HAT
+#
+###############################################################################
+
+Class BEBigGameHuntersHat : BEHelmet {
+	BEBigGameHuntersHat() : base() {
+		$this.Name               = 'Big Game Hunter''s Hat'
+		$this.MapObjName         = 'biggamehuntershat'
+		$this.PurchasePrice      = 250
+		$this.SellPrice          = 125
+		$this.TargetStats        = @{
+			[StatId]::Defense = 8
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A distinctive hat worn by big game hunters, indicating their prowess.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::Defense]) DEF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
