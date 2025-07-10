c3114d743ea90f803f11a97a92dff170fa87abbc
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:30:27 -0400

    Added BEWritersQuillCap

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEWritersQuillCap.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE WRITER'S QUILL CAP
+#
+###############################################################################
+
+Class BEWritersQuillCap : BEHelmet {
+	BEWritersQuillCap() : base() {
+		$this.Name               = 'Writer''s Quill Cap'
+		$this.MapObjName         = 'writersquillcap'
+		$this.PurchasePrice      = 80
+		$this.SellPrice          = 40
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 2
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A cap with a quill, aiding writers in their craft.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
