152f1b6b64a10f75472b59e0c2b43272dd9460ca
Gregory F Martin <greg@gregfmartin.org>
2025-07-10 09:29:59 -0400

    Added BEPoetsLaurelWreath

---
+++ Classes/CombatEnginePrimitives/Equipment/Helmets/BEPoetsLaurelWreath.ps1
@@ -0,0 +1,25 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# BE POET'S LAUREL WREATH
+#
+###############################################################################
+
+Class BEPoetsLaurelWreath : BEHelmet {
+	BEPoetsLaurelWreath() : base() {
+		$this.Name               = 'Poet''s Laurel Wreath'
+		$this.MapObjName         = 'poetslaurelwreath'
+		$this.PurchasePrice      = 90
+		$this.SellPrice          = 45
+		$this.TargetStats        = @{
+			[StatId]::MagicDefense = 3
+		}
+		$this.CanAddToInventory  = $true
+		$this.ExamineString      = 'A laurel wreath worn by poets, inspiring eloquence.'
+		$this.PlayerEffectString = "+$($this.TargetStats[[StatId]::MagicDefense]) MDF"
+		$this.TargetGender       = [Gender]::Unisex
+	}
+}
