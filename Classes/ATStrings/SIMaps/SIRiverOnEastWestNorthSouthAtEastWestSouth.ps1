2606c4bd5bcfbb7e419daa951262f3349fefa118
Gregory F Martin <greg@gregfmartin.org>
2025-07-11 10:26:51 -0400

    Added SIRiverOnEastWestNorthSouthAtEastWestSouth

---
+++ Classes/ATStrings/SIMaps/SIRiverOnEastWestNorthSouthAtEastWestSouth.ps1
@@ -0,0 +1,13 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# SI RIVER ON EAST WEST NORTH SOUTH AT EAST WEST SOUTH
+#
+###############################################################################
+
+Class SIRiverOnEastWestNorthSouthAtEastWestSouth : SIInternalBase {
+    SIRiverOnEastWestNorthSouthAtEastWestSouth() : base("$(Get-Location)\Image Data\SIRiverOnEastWestNorthSouthAtEastWestSouth.json") {}
+}
