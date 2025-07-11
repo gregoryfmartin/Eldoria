bb4188da1f646e6fb23fc224a74c424d98c7b3a6
Gregory F Martin <greg@gregfmartin.org>
2025-07-11 11:03:38 -0400

    Added SIRiverOnNorthSouthEastAtNorth

---
+++ Classes/ATStrings/SIMaps/SIRiverOnNorthSouthEastAtNorth.ps1
@@ -0,0 +1,13 @@
+using namespace System
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# SI RIVER ON NORTH SOUTH EAST AT NORTH
+#
+###############################################################################
+
+Class SIRiverOnNorthSouthEastAtNorth : SIInternalBase {
+    SIRiverOnNorthSouthEastAtNorth() : base("$(Get-Location)\Image Data\SIRiverOnNorthSouthEastAtNorth.json") {}
+}
