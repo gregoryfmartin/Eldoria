bf5d3cef5a1a092536413703412041b669a79509
Gregory F Martin <greg@gregfmartin.org>
2025-07-11 08:51:06 -0400

    Added BattlePhaseIndicator

--- Eldoria.psm1
+++ Eldoria.psm1
@@ -143,6 +143,7 @@ Foreach($File in (Get-ChildItem -Path "$PSScriptRoot\Classes\CombatEnginePrimiti
 . "$PSScriptRoot\Classes\BufferManager.ps1"
 . "$PSScriptRoot\Classes\UI\UIEContainer.ps1"
 . "$PSScriptRoot\Classes\UI\WindowBase.ps1"
+. "$PSScriptRoot\Classes\UI\BattlePhaseIndicator.ps1"
 
 Foreach($File in (Get-ChildItem -Path "$PSScriptRoot\Classes\UI\Windows\*.ps1")) {
     . $File.FullName
