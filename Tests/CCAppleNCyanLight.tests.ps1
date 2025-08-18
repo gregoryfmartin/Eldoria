ad839467f340625110d8cee09c991f3791b6b2ef
Gregory F Martin <greg@gregfmartin.org>
2025-08-17 22:02:26 -0400

    Added Apple N Cyan Light tests

---
+++ Tests/CCAppleNCyanLight.tests.ps1
@@ -0,0 +1,26 @@
+Describe 'CCAppleNCyanLight24' {
+    BeforeAll {
+        . $PSScriptRoot\..\Classes\ConsoleColor\ConsoleColor24.ps1
+        . $PSScriptRoot\..\Classes\ConsoleColor\CCAppleNCyanLight.ps1
+    }
+
+    BeforeEach {
+        $Sample = [CCAppleNCyanLight24]::new()
+    }
+
+    AfterEach {
+        $Sample = $null
+    }
+
+    Context 'Initializtion' {
+        It 'Should create a new instance of CCAppleNCyanLight24' {
+            $Sample.PSTypeNames | Should -Contain 'CCAppleNCyanLight24'
+        }
+
+        It 'Should have populated channels' {
+            $Sample.Red | Should -Be 50
+            $Sample.Green | Should -Be 173
+            $Sample.Blue | Should -Be 230
+        }
+    }
+}
