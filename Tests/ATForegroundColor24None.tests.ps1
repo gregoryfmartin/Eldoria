59f686e1ce1475e9f4e5f7fdcca12a72b95304ae
Gregory F Martin <greg@gregfmartin.org>
2025-08-19 12:40:24 -0400

    Added ATForegroundColor24None tests

--- Tests/ATForegroundColor24None.tests.ps1
+++ Tests/ATForegroundColor24None.tests.ps1
@@ -1,24 +1,24 @@
-Describe 'ATBackgroundColor24None' {
+Describe 'ATForegroundColor24None' {
     BeforeAll {
         . $PSScriptRoot\..\Classes\ConsoleColor\ConsoleColor24.ps1
         . $PSScriptRoot\..\Classes\ConsoleColor\CCBlack.ps1
         . $PSScriptRoot\..\Classes\ATStrings\ATControlSequences.ps1
-        . $PSScriptRoot\..\Classes\ATStrings\ATBackgroundColor.ps1
-        . $PSScriptRoot\..\Classes\ATStrings\ATBackgroundColorNone.ps1
+        . $PSScriptRoot\..\Classes\ATStrings\ATForegroundColor.ps1
+        . $PSScriptRoot\..\Classes\ATStrings\ATForegroundColorNone.ps1
     }
 
     Context 'Constructor' {
         It 'Should create an instance and correctly store the color object' {
-            [ATBackgroundColor24None]$Sample = [ATBackgroundColor24None]::new()
+            [ATForegroundColor24None]$Sample = [ATForegroundColor24None]::new()
 
-            $Sample.PSTypeNames | Should -Contain 'ATBackgroundColor24None'
+            $Sample.PSTypeNames | Should -Contain 'ATForegroundColor24None'
         }
     }
 
     Context 'ToAnsiControlSequenceString Method' {
         It 'Should return the exact string providing the color information' {
-            [ATBackgroundColor24None]$Sample = [ATBackgroundColor24None]::new()
-            [String]$ExpectedString      = ''
+            [ATForegroundColor24None]$Sample = [ATForegroundColor24None]::new()
+            [String]$ExpectedString          = ''
 
             $Sample.ToAnsiControlSequenceString() | Should -Be $ExpectedString
         }
