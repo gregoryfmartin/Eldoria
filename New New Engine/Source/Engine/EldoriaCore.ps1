553b322aaeb379f6c4e006b7fc3692380d5d641f
Gregory F Martin <greg@gregfmartin.org>
2026-08-01 00:07:28 -0400

    Forgot the cleanup

--- New New Engine/Source/Engine/EldoriaCore.ps1
+++ New New Engine/Source/Engine/EldoriaCore.ps1
@@ -22,33 +22,6 @@ Class EldoriaCore : GameCore {
 
         $this.GameState.AddState([GSSplashScreenA]::new())
 
-        # $this.GameState.AddState(
-        #     [SMState]::new(
-        #         'SplashScreenA',
-        #         {
-        #             Write-Host 'Entered Splash Screen A State'
-        #         }, # ENTER WILL BE CALLED HERE SINCE IT'S COMING FROM INIT
-        #         {
-        #             Write-Host 'Leaving the Splash Screen A State'
-        #         }, # EXITING THIS STATE ISN'T YET WELL DEFINED
-        #         {
-        #             Param(
-        #                 [Context]$Context
-        #             )
-
-        #             # CONTEXT LAYOUT HERE IS
-        #             # 0 - DELTA TIME
-        #             # 1 - KEYS PRESSED
-        #             # 2 - SELF
-        #             If($Context.References[1].Count -GT 0) {
-        #                 Foreach($KeyPress in $Context.References[1]) {
-        #                     Write-Host "$($KeyPress.Key.ToString())"
-        #                 }
-        #             }
-        #         }
-        #     )
-        # )
-
         $this.GameState.AddTransition(
             [SMTransition]::new(
                 'GSInit',
